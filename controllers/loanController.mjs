99071580be0f1e80a65dcfadc8c2aee8e46763bf
piragna <gonzalovarelaalagna@gmail.com>
2025-08-16 21:46:41 -0300

    New methods: - calculateRepaymentCapacity - calculateMaxLoanCapacity

--- controllers/loanController.mjs
+++ controllers/loanController.mjs
@@ -1,3 +1,7 @@
+const {calculateLoanDetails} = import ('../utils/services/loanService.js');
+
 export function calculateLoan(req,res){
     //extract input, call utility function, return result
+    const result = calculateLoanDetails(req.body);
+    res.json(result);
 };
---
+++ utils/services/loanService.js
@@ -0,0 +1,38 @@
+const loan = {
+    principal:100000, //amount borrowed,
+    interestRate:5.0, //anual interest rate in %
+    termYears:15, //duration of loan in years
+    paymentFrecuency: 'monthly', //monthly, biweekly...
+    startDate: '2025-08-01' //loan start date    
+};
+
+//loan object:
+const interestRate=0.25; //is in object
+const loanTermYears = 1; //is in obj
+const interestBuffer = 0.25;// ins in obj
+
+//user data:
+const user = {
+    annualSalary:60000,
+    paymentRatio : 0.20
+};
+
+function calculateRepaymentCapacity(user, loanTermYears){
+    const annualRepaymentCapacity = user.annualSalary*user.paymentRatio; //discounting the interest to the annual salary
+    //annualRepaymentCapacity * years = maxRepaymentCapacity
+    const repaymentCapacity = annualRepaymentCapacity*loanTermYears;
+    console.log(`User repayment capacity over ${loanTermYears} years: $${repaymentCapacity.toLocaleString()}`);
+    return repaymentCapacity;
+}
+
+function calculateMaxLoanCapacity(repaymentCapacity, loanTermYears, interestRate){
+    //maxium affordable price->repaymentCapacity
+    const totalRepaymentCapacity = repaymentCapacity;
+    const maxLoanCapacity = totalRepaymentCapacity/(1+interestRate);
+    console.log(`User max loan capacity over ${loanTermYears} years: $${maxLoanCapacity.toLocaleString()} with an interest rate of % ${interestRate*100}.`);
+    return maxLoanCapacity;
+};
+
+//loanTermYears=1 
+//interestRate=0.25
+calculateMaxLoanCapacity(calculateRepaymentCapacity(user,loanTermYears),loanTermYears,interestRate);//User max loan capacity over 1 years: $9,000
