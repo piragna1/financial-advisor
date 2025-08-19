73fc1a9587dec4c7f47ce3beac5e822c1a0b296c
piragna <gonzalovarelaalagna@gmail.com>
2025-08-19 15:47:50 -0300

    Creating userRepo.js file

---
+++ src/repositories/userRepo.js
@@ -0,0 +1,12 @@
+// userRepo.js
+
+const users = [] // In-memory store for now
+
+export async function findUserByEmail(email) {
+  return users.find(user => user.email === email)
+}
+
+export async function saveUser(user) {
+  users.push(user)
+  return user
+}
