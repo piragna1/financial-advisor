6a4b4bcce5d9ba10410fc4d245e4c8bb55a0b4b3
piragna <gonzalovarelaalagna@gmail.com>
2025-08-20 11:38:05 -0300

    ✔️ 'loginUserController' function created on authController 🎮

--- src/controllers/authController.mjs
+++ src/controllers/authController.mjs
@@ -1,6 +1,7 @@
 // src/controllers/authController.mjs
 
 import { registerUser } from '../services/authService.js'
+import { loginUser } from '../services/authService.js'
 
 export async function registerUserController(req, res) {
   try {
@@ -10,5 +11,15 @@ export async function registerUserController(req, res) {
     res.status(201).json({ id: newUser.id })
   } catch (error) {
     res.status(400).json({ error: error.message })
+  }
+}
+
+export async function loginUserController(req, res) {
+  try {
+    const {email,password} = req.body;
+    const loggedUser = loginUser({email,password});
+    res.status(200).json({ token });
+  } catch (error) {
+    res.status(401).json({error: error.message})
   }
 }
