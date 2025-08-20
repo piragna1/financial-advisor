e15e2b024179f49ac1fec4aedc277805a4c8c22a
piragna <gonzalovarelaalagna@gmail.com>
2025-08-20 13:40:53 -0300

    JWT auth simulation:
    functions created:
    -generateToken()
    -verifyToken()

--- src/utils/token.js
+++ src/utils/token.js
@@ -1,9 +1,26 @@
-export function generateToken({userId}){
+export function generateToken(userId, secret = 'secret',expiresInSeconds='3600'){
+    Math.floor(Date.now()/1000)+expiresInSeconds;
+    const header = {alg:'HS256', typ:'JWT'};
     const payload = {
         sub:userId,
         iat:Date.now()
     };
     //simulate encoding
-    const base64 = Buffer.from(JSON.stringify(payload)).toString('base64');
-    return `simulated-jwt.${base64}`;
+    const base64Header = Buffer.from(JSON.stringify(header)).toString('base64');
+    const base64Payload = Buffer.from(JSON.stringify(payload)).toString('base64');
+    const signature = Buffer.from(base64Header+'.'+base64Payload+secret).toString('base64')
+    return `${base64Header}.${base64Payload}.${signature}`;
 };
+
+export function verifyToken(token,secret='secret'){
+    const [headerB64, payloadB64, signatureB64] = token.split('.');
+    if (!headerB64 || !payloadB64 || !signatureB64){
+        throw new Error('Invalid token structure');
+    }
+    const expectedSignature = Buffer.from(headerB64+'.'+payloadB64+secret).toString('base64url');
+    if (signatureB64 !== expectedSignature){
+        throw new Error('Invalid token signature.');
+    }
+    const payload = JSON.parse(Buffer.from(payloadB64, 'base64url').toString());
+    return payload;
+}
