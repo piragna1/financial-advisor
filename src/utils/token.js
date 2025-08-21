f2f74d9aa8c809ada3361433920dc7b73426aa8e
piragna <gonzalovarelaalagna@gmail.com>
2025-08-21 10:10:20 -0300

    token.js file: +generateToken method:  -modified encoding formate  -modified import  +verifyToken method:   -update second argument's default value

--- src/utils/token.js
+++ src/utils/token.js
@@ -1,4 +1,5 @@
-export function generateToken(userId, secret = 'secret',expiresInSeconds=3600){
+import { generateSignature } from "./hashUtil";
+export function generateToken(userId, secret = 'simulationSecret',expiresInSeconds=3600){
     const now = Math.floor(Date.now()/1000);
     const exp = now+expiresInSeconds;
     const header = {alg:'HS256', typ:'JWT'};
@@ -7,13 +8,13 @@ export function generateToken(userId, secret = 'secret',expiresInSeconds=3600){
         iat:now,
         exp
     };
-    const base64Header = Buffer.from(JSON.stringify(header)).toString('base64');
-    const base64Payload = Buffer.from(JSON.stringify(payload)).toString('base64');
-    const signatureB64 = Buffer.from(`${base64Header}.${base64Payload+secret}`).toString('base64');
+    const base64Header = Buffer.from(JSON.stringify(header)).toString('base64url');
+    const base64Payload = Buffer.from(JSON.stringify(payload)).toString('base64url');
+    const signatureB64 = Buffer.from(`${base64Header}.${base64Payload+secret}`).toString('base64url');
     return `${base64Header}.${base64Payload}.${signatureB64}`;
 };
 
-export function verifyToken(token,secret='secret'){
+export function verifyToken(token,secret='simulationSecret'){
     const [headerB64, payloadB64, signatureB64] = token.split('.');
     if (!headerB64 || !payloadB64 || !signatureB64){
         throw new Error('Invalid token structure');
@@ -29,6 +30,9 @@ export function verifyToken(token,secret='secret'){
 export function decodeToken(token){
     try {
         const decoded = JSON.parse(Buffer.from(token, 'base64').toString());
+        // const payloadB64 = token.split('.')[1];
+        // const payload = JSON.parse(atob(token.split('.')[1]));
+        // const payload = JSON.parse(Buffer.from(payloadB64, 'base64').toString());
         return decoded;
     } catch (error) {
         return null;
@@ -36,7 +40,7 @@ export function decodeToken(token){
 }
 
 export function isTokenExpired(token){
-    const payload = decodeToken(token);
+    const payload = decodeToken(token); //check return value
     if(!payload || !payload.exp) return true;
     const now = Math.floor(Date.now()/100);
     return now > payload.exp;
