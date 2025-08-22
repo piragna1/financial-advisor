d41d35d11dbd960d7bd60f29f6e6acfb5f48a2a7
piragna <gonzalovarelaalagna@gmail.com>
2025-08-22 15:52:13 -0300

    changes:
    -lastName attribute added,
    -isActive attribue deleted

--- src/models/userModel.js
+++ src/models/userModel.js
@@ -1,8 +1,8 @@
 const User = {
   id: String,               // Unique identifier
   name: String,             // Provided at sign-up
+  lastName:String,
   email: String,            // Used for login and uniqueness
   passwordHash: String,     // Hashed password for security
   createdAt: Date,          // Timestamp of account creation
-  isActive: Boolean        // Optional: for future activation flows
 };
