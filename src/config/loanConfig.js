a84abf59d19160c964bdb4833e83a6215df15671
piragna <gonzalovarelaalagna@gmail.com>
2025-08-22 15:47:26 -0300

    defining data

--- src/config/loanConfig.js
+++ src/config/loanConfig.js
@@ -2,6 +2,6 @@ export const loan = {
     principal:100000, //amount borrowed,
     interestRate:5.0, //anual interest rate in %
     termYears:15, //duration of loan in years
-    paymentFrecuency: 1, //monthly, biweekly... integer month number per year
-    startDate: '2025-08-01' //loan start date    
+    paymentFrecuency: 1, //payments per year
+    startDate: '2025-08-01' //loan start date
 };
