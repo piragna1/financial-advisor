1fb495fce94b2a3148b694ee15b2b65fe716b558
piragna <gonzalovarelaalagna@gmail.com>
2025-08-25 13:56:41 -0300

    This function was impure. Now it is a pure actor.

--- src/actors/validators/auth/checkUserUniqueness.js
+++ src/actors/validators/auth/checkUserUniqueness.js
@@ -1,6 +1,8 @@
-import { listUsers } from "../repositories/userRepo";
-
-export async function checkUserUniqueness (email) {
-    const users = listUsers();
-    return users.some(u => u.email === email);
+//now receives users as argument in order to not call userRepo and being an impure function.
+export async function checkUserUniqueness (email, users) {
+    const exist = users.some((user) => u.email === email);
+    return {
+        succes: exist,
+        message: exist ? 'Email is not valid' : 'Email available'
+    }
 }
