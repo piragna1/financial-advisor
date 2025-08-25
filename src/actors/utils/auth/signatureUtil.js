4703d00c19891dc0ec7083323eb588ccbc09fd2b
piragna <gonzalovarelaalagna@gmail.com>
2025-08-25 15:39:15 -0300

    decoupled hash function

--- src/actors/utils/auth/signatureUtil.js
+++ src/actors/utils/auth/signatureUtil.js
@@ -5,17 +5,7 @@ export function generateSignature(string, secret) {
   if (typeof secret !== "string" || secret.length === 0) {
   throw Error("Invalid secret");
 }
-  let signature = "";
-  const prime1 = 3;
-  const prime3= 199;
+  let signature = hash(string,secret);
 
-  let sum;
-  for (let index = 0; index < string.length; index++) {
-    sum=0;
-    let charcode = string.charCodeAt(index);
-    let charcode1=secret.charCodeAt(index%secret.length);
-    sum += ((charcode*charcode1*prime3)*prime1);
-    signature+=sum.toString();
-  }
   return Buffer.from(signature.slice(0,256)).toString('base64url');
 }
