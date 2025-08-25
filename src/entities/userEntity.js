af4853b213141f4a2f94fbbce51fe4519eebb52f
piragna <gonzalovarelaalagna@gmail.com>
2025-08-25 16:10:41 -0300

    fixed returned element

--- src/entities/userEntity.js
+++ src/entities/userEntity.js
@@ -1,7 +1,4 @@
 
 export function buildUserEntity({id,name,lastName,email,hashedPassword},){
-    //consider all values as valid
-    return {
-        id,name,lastName,email,hashedPassword
-    };
+    return arguments[0];
 };
