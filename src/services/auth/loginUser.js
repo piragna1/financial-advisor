13da832391b64123fa753c35d0b1ee7387565c89
piragna <gonzalovarelaalagna@gmail.com>
2025-08-25 12:01:15 -0300

    File created

---
+++ src/services/auth/loginUser.js
@@ -0,0 +1,11 @@
+import { findUserByEmail } from "../../repositories/userRepo";
+import { generateToken } from "../../utils/token";
+import { validateLoginInput } from "../../validators/validateLoginInput";
+
+export async function loginUser({ email, password }) {
+  validateLoginInput(email, password);
+  const user = await findUserByEmail(email);
+  await verifyPassword(password, user.passwordHash);
+  const token = generateToken({ userId: user.id });
+  return { user, token };
+}
