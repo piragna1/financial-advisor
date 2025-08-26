1df62427990319983b0c1e1c95ed893b0860015e
piragna <gonzalovarelaalagna@gmail.com>
2025-08-26 12:49:12 -0300

    fixes

--- src/services/auth/loginUser.js
+++ src/services/auth/loginUser.js
@@ -1,27 +1,24 @@
-import { generateToken } from "../../utils/token";
-import {userRetrieve} from '../../actors/retrievers/userRetriever.js'
-import {validateLoginInput} from '../../actors/validators/auth/validateLoginInput.js'
-import {comparePasswords} from '../../actors/security/comparePasswords.js'
-import {} from ''
+import { userRetrieve } from "../../actors/retrievers/userRetriever.js";
+import { validateLoginInput } from "../../actors/validators/auth/validateLoginInput.js";
+import { comparePasswords } from "../../utils/auth/comparePasswords.js";
+import { issueToken } from "../../utils/auth/tokenIssuer.js";
 
 export async function loginUser({ email, password }) {
-
-  const valid = validateLoginInput(email, password);
+  const valid = validateLoginInput({email, password});
+  if (!valid) throw {code:'INVALID_INPUT'} 
   console.log(valid);
 
-  const user = userRetrieve(email);
+  const user = await userRetrieve(email); 
+  if (!user) throw { code: "USER_NOT_FOUND" };
   console.log(user);
 
-
-  const validPass = comparePasswords(user.passwordHash,password);
+  const validPass = comparePasswords(user.passwordHash, password);
   console.log(validPass);
+  if (!validPass) throw {code: "INVALID_CREDENTIALS",};
 
-
-  const token = generateToken({ userId: user.id });
+  const token = await issueToken(user);
   console.log(token);
+  console.log({ user, token });
 
-
-  console.log({user,token});
-
-  return { user, token };
+  return { user, token, success: true };
 }
