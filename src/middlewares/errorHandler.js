4a6344e84286cb16a21be1351e9ea2da5349bcad
piragna <gonzalovarelaalagna@gmail.com>
2025-08-26 14:28:29 -0300

    error handling

--- src/middlewares/errorHandler.js
+++ src/middlewares/errorHandler.js
@@ -1,1 +1,11 @@
-export function errorHandler(err,req,res,next){}
+export function errorHandler(err,req,res,next){
+    if (err.status){
+        return res.status(err.status).json({
+            error:err.mesagge,
+            code:err.code,
+            details:err.details
+        });
+    }
+    console.error(err);
+    res.status(500).json({error:'Internal server error'});
+}
--- src/services/auth/loginUser.js
+++ src/services/auth/loginUser.js
@@ -1,20 +1,22 @@
 import { userRetrieve } from "../../actors/retrievers/userRetriever.js";
 import { validateLoginInput } from "../../actors/validators/auth/validateLoginInput.js";
+import { AppError } from "../../errors/AppError.js";
+import { AuthErrors } from "../../errors/authErrors.js";
 import { comparePasswords } from "../../utils/auth/comparePasswords.js";
 import { issueToken } from "../../utils/auth/tokenIssuer.js";
 
 export async function loginUser({ email, password }) {
   const valid = validateLoginInput({email, password});
-  if (!valid) throw {code:'INVALID_INPUT'} 
+  if (!valid) throw new AppError(AuthErrors.INVALID_INPUT);
   console.log(valid);
 
   const user = await userRetrieve(email); 
-  if (!user) throw { code: "USER_NOT_FOUND" };
+  if (!user) throw new AppError(AuthErrors.USER_NOT_FOUND);
   console.log(user);
 
   const validPass = comparePasswords(user.passwordHash, password);
   console.log(validPass);
-  if (!validPass) throw {code: "INVALID_CREDENTIALS",};
+  if (!validPass) throw new AppError(AuthErrors.INVALID_CREDENTIALS);
 
   const token = await issueToken(user);
   console.log(token);
