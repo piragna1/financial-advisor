702047d79a4f5dc9c5e1cf2c12e820ba87e9ff94
piragna <gonzalovarelaalagna@gmail.com>
2025-08-26 10:43:15 -0300

    removed comment

--- src/actors/validators/auth/checkUserUniqueness.js
+++ src/actors/validators/auth/checkUserUniqueness.js
@@ -1,4 +1,3 @@
-//now receives users as argument in order to not call userRepo and being an impure function.
 export async function checkUserUniqueness (email, users) {
     const exist = users.some((user) => u.email === email);
     return {
