0035b6ea2036604ad2883b9140d1e6761e1501b0
piragna <gonzalovarelaalagna@gmail.com>
2025-08-27 19:56:14 -0300

    Logs removed.

--- src/services/auth/loginUser.js
+++ src/services/auth/loginUser.js
@@ -6,24 +6,17 @@ import { comparePasswords } from "../../utils/auth/comparePasswords.js";
 import { issueToken } from "../../utils/auth/tokenIssuer.js";
 
 export async function loginUser({ email, password }) {
-  console.log('loginUser.js -> loginUser()')
 
   const valid = validateLoginInput({email, password});
-  console.log('valid', valid);
   if (!valid.ok) throw new AppError(AuthErrors.INVALID_INPUT);
 
   const user = await userRetrieve(email);
-  console.log('user', user);
   if (!user) throw new AppError(AuthErrors.USER_NOT_FOUND);
-  console.log('user:',user);
 
   const validPass = comparePasswords(user.passwordHash, password);
-  console.log(validPass);
   if (!validPass) throw new AppError(AuthErrors.INVALID_CREDENTIALS);
 
   const token = await issueToken(user);
-  console.log(token);
-  console.log({ user, token });
 
   return { user, token, success: true };
 }
