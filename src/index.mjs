a07de7cf5c74040cefd25dcb2d6806122995de01
piragna <gonzalovarelaalagna@gmail.com>
2025-08-27 09:16:06 -0300

    Fix import

--- src/index.mjs
+++ src/index.mjs
@@ -1,7 +1,7 @@
 //index.mjs
 import express from 'express';
 import authRoutes from '../routes/auth/auth.route.js';
-import {errorHandler} from './middlewares/errorHandler.js'
+import errorHandler from './middlewares/errorHandler.js'
 
 const PORT = 3000;
 
@@ -9,17 +9,15 @@ const app = express();
 
 app.use(express.json());
 
-// app.use('/auth', authRoutes);
-
-// app.post('/login', authRoutes);
-
-// app.use(errorHandler);
+app.use('/auth', authRoutes);
 
 app.get('/', (req,res)=>{
-    res.send('<h1>Express server active</h1>');
+    res.send(`<h1 style='color:red'>Express server active</h1><p>Hola que tal soy un parrafado</p>`);
 });
+
+
+app.use(errorHandler);
 
 app.listen(PORT, () =>{
     console.log(`Local server running on: http://localhost:${PORT}`);
 })
-
