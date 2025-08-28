d8ba04465e3d4ebcc9bfda982a68b4c18b6a6563
piragna <gonzalovarelaalagna@gmail.com>
2025-08-28 09:12:39 -0300

    1st implementation

--- src/actors/security/auth/verifyToken.js
+++ src/actors/security/auth/verifyToken.js
@@ -1,3 +1,15 @@
-export function verifyToken(token){
-    
+
+export function verifyToken(token, secret = "simulationSecret") {
+  const [base64Header, base64Payload, base64Signature] = token.split(".");
+  if (!base64Header || !base64Payload || !base64Signature) {
+    throw new Error("Invalid token structure");
+  }
+  const expectedSignature = generateSignature(
+    `${base64Header}.${base64Payload}`,
+    secret
+  );
+  if (base64Signature !== expectedSignature) {
+    throw new Error("Invalid token signature.");
+  }
+  return true;
 }
--- src/utils/auth/token.js
+++ src/utils/auth/token.js
@@ -25,20 +25,6 @@ export async function generateToken(
   return `${base64Header}.${base64Payload}.${base64Signature}`;
 }
 
-export function verifyToken(token, secret = "simulationSecret") {
-  const [base64Header, base64Payload, base64Signature] = token.split(".");
-  if (!base64Header || !base64Payload || !base64Signature) {
-    throw new Error("Invalid token structure");
-  }
-  const expectedSignature = generateSignature(
-    `${base64Header}.${base64Payload}`,
-    secret
-  );
-  if (base64Signature !== expectedSignature) {
-    throw new Error("Invalid token signature.");
-  }
-  return true;
-}
 
 export function decodePayload(token) {
   try {
