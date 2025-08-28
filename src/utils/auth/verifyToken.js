42c05120ee9a7dfd61234d8f9a4aae2754775feb
piragna <gonzalovarelaalagna@gmail.com>
2025-08-28 09:50:43 -0300

    Changes: -Handling expiration error -Moved file to utils

--- src/utils/auth/verifyToken.js
+++ src/utils/auth/verifyToken.js
@@ -1,5 +1,6 @@
-import { AppError } from "../../../errors/AppError";
-import {generateSignature} from '../../../actors/utils/auth/generateSignature.js'
+import { AppError } from "../../errors/AppError.js";
+import {generateSignature} from '../../actors/utils/auth/generateSignature.js'
+import { decodePayload, isTokenExpired } from "./token.js";
 
 export function verifyToken(token, secret = "simulationSecret",req) {
   const [base64Header, base64Payload, base64Signature] = token.split(".");
@@ -16,8 +17,12 @@ export function verifyToken(token, secret = "simulationSecret",req) {
   if (base64Signature !== expectedSignature) {
     throw new AppError(TokenErrors.INVALID_SIGNATURE);
   }
-  
-  req.user.payload = JSON.parse(Buffer.from(base64Payload, 'base64url').toString());
 
-  return true;
+  const decoded = decodePayload(token);
+
+  if (isTokenExpired(decoded)) throw new AppError(TokenErrors.EXPIRED_TOKEN);
+  
+  req.user.payload = decoded;
+
+  next(req);
 }
