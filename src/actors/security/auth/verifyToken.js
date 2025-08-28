b0c2e5b2aa2863199551a07ced8bc6334faefb2a
piragna <gonzalovarelaalagna@gmail.com>
2025-08-28 09:45:06 -0300

    Attaching payload to req.user

--- src/actors/security/auth/verifyToken.js
+++ src/actors/security/auth/verifyToken.js
@@ -1,7 +1,7 @@
 import { AppError } from "../../../errors/AppError";
 import {generateSignature} from '../../../actors/utils/auth/generateSignature.js'
 
-export function verifyToken(token, secret = "simulationSecret") {
+export function verifyToken(token, secret = "simulationSecret",req) {
   const [base64Header, base64Payload, base64Signature] = token.split(".");
 
   if (!base64Header) {throw new Error(TokenErrors.INVALID_HEADER);}
@@ -17,5 +17,7 @@ export function verifyToken(token, secret = "simulationSecret") {
     throw new AppError(TokenErrors.INVALID_SIGNATURE);
   }
   
+  req.user.payload = JSON.parse(Buffer.from(base64Payload, 'base64url').toString());
+
   return true;
 }
