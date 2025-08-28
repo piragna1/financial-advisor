355c0ae6947bab4cc17a5a59f01854bfab770a26
piragna <gonzalovarelaalagna@gmail.com>
2025-08-28 09:29:17 -0300

    Error handling in verifyToken();

--- src/actors/security/auth/verifyToken.js
+++ src/actors/security/auth/verifyToken.js
@@ -1,16 +1,21 @@
 import { AppError } from "../../../errors/AppError";
+import {generateSignature} from '../../../actors/utils/auth/generateSignature.js'
 
 export function verifyToken(token, secret = "simulationSecret") {
   const [base64Header, base64Payload, base64Signature] = token.split(".");
-  if (!base64Header) {throw new Error("Invalid token structure");}
-  if (!base64Payload) {throw new Error("Invalid token structure");}
+
+  if (!base64Header) {throw new Error(TokenErrors.INVALID_HEADER);}
+  if (!base64Payload) {throw new Error(TokenErrors.INVALID_PAYLOAD);}
   if (!base64Signature) {throw new AppError(TokenErrors.INVALID_SIGNATURE);}
+
   const expectedSignature = generateSignature(
     `${base64Header}.${base64Payload}`,
     secret
   );
+
   if (base64Signature !== expectedSignature) {
-    throw new Error("Invalid token signature.");
+    throw new AppError(TokenErrors.INVALID_SIGNATURE);
   }
+  
   return true;
 }
