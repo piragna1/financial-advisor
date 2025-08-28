d24d8066729595705cb91d2c471894037c8100f7
piragna <gonzalovarelaalagna@gmail.com>
2025-08-28 15:59:37 -0300

    Creating middleware for token authentications

---
+++ src/middlewares/authMiddleware.js
@@ -0,0 +1,17 @@
+import {extractToken} from '../actors/utils/auth/extractToken.js'
+import {tokenExists} from '../actors/utils/auth/tokenExists.js'
+import { AppError } from '../errors/AppError.js';
+import { TokenErrors } from '../errors/tokenError.js';
+import { verifyToken } from '../utils/auth/verifyToken.js';
+
+export function authMiddleware(err,res,req,next){
+    if (!tokenExists(req)) {
+        throw new AppError(TokenErrors.MISSING_TOKEN);
+    }
+    const token = extractToken(req);
+    const valid = verifyToken(token);
+    if(valid) next();
+    else{
+        throw new AppError(TokenErrors)
+    }
+}
