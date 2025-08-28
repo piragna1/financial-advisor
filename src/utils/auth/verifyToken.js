a0186d8a6468b1d4fca65d96a7a37d5d23bf411e
piragna <gonzalovarelaalagna@gmail.com>
2025-08-28 14:30:38 -0300

    Update imports

--- src/utils/auth/verifyToken.js
+++ src/utils/auth/verifyToken.js
@@ -1,12 +1,13 @@
 import { AppError } from "../../errors/AppError.js";
 import {generateSignature} from '../../actors/utils/auth/generateSignature.js'
 import { decodePayload, isTokenExpired } from "./token.js";
-import {TokenErrors} from '../../errors/tokenError.js'
+import {TokenErrors} from '../../errors/tokenError.js';
+import {extractToken} from '../../actors/utils/auth/extractToken.js';
 
 export function verifyToken(req,res,next,secret = "simulationSecret") {
 
   if (!req.token) throw new AppError(TokenErrors.MISSING_TOKEN);
-  const token = req.token;
+  const token = extractToken(req);
 
   const [base64Header, base64Payload, base64Signature] = token.split(".");
   if (!base64Header) {throw new AppError(TokenErrors.INVALID_HEADER);}
