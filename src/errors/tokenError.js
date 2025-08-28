757900df8db16b6b1c4dcecc5274a5ed42665ac5
piragna <gonzalovarelaalagna@gmail.com>
2025-08-28 09:17:56 -0300

    New errors object literal.

---
+++ src/errors/tokenError.js
@@ -0,0 +1,11 @@
+const tokenError = {
+    INVALID_HEADER:{
+        message:'Invalid token'
+    },
+    INVALID_PAYLOAD:{
+        message:'Invalid token'
+    },
+    INVALID_SIGNATURE:{
+        message:'Invalid token'
+    },
+}
