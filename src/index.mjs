16cb381384d4773811784ccfabccea78fcdc85ef
piragna <gonzalovarelaalagna@gmail.com>
2025-09-01 12:18:45 -0300

    Update index

--- src/index.mjs
+++ src/index.mjs
@@ -1,7 +1,8 @@
-//index.mjs
+//index.mjs;
 import express from 'express';
-import authRoutes from '../routes/auth/auth.route.js';
-import errorHandler from './middlewares/errorHandler.js'
+import authRoutes from './routes/auth/auth.route.js';
+import profileRoutes from './routes/profile/profile.route.js';
+import errorHandler from './middlewares/errorHandler.js';
 
 const PORT = 3000;
 
@@ -11,13 +12,14 @@ app.use(express.json());
 
 app.use('/auth', authRoutes);
 
+app.use('/profile', profileRoutes);
+
 app.get('/', (req,res)=>{
     res.send(`<h1 style='color:red'>Express server active</h1><p>Hola que tal soy un parrafado</p>`);
 });
-
 
 app.use(errorHandler);
 
 app.listen(PORT, () =>{
     console.log(`Local server running on: http://localhost:${PORT}`);
-})
+});
