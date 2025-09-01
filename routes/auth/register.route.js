d02584c5488b8fafc783f7885b01ac297d54bdde
piragna <gonzalovarelaalagna@gmail.com>
2025-09-01 10:54:53 -0300

    Deleted files

--- routes/auth/register.route.js
+++
@@ -1,5 +0,0 @@
-const express = import ('express');
-import { registerUserController } from '../../src/controllers/authController.mjs';
-const router= express.Router();
-router.post(('/register'),registerUserController);
-export default router;
