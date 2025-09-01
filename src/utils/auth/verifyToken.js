9f9c709e5ced7e3c6471dc7cc49e2e21684d88f7
piragna <gonzalovarelaalagna@gmail.com>
2025-09-01 20:34:16 -0300

    fixing

--- src/utils/auth/verifyToken.js
+++ src/utils/auth/verifyToken.js
@@ -1,25 +1,28 @@
 import { AppError } from "../../errors/AppError.js";
 import {generateSignature} from '../../actors/utils/auth/generateSignature.js'
-import { decodePayload, isTokenExpired } from "./token.js";
+import { decodePayload, generateToken, isTokenExpired } from "./token.js";
 import {TokenErrors} from '../../errors/tokenError.js';
 
-export function verifyToken(token,secret = "simulationSecret") {
+export async function verifyToken(token) {
 
   const [base64Header, base64Payload, base64Signature] = token.split(".");
+
+  base64Header.splice(6);
+  console.log('base 64 Header', base64Header)
+  // console.log('base 64 Payload', base64Payload)
+  // console.log('base 64 Signature', base64Signature)
+  
   if (!base64Header) {throw new AppError(TokenErrors.INVALID_HEADER);}
   if (!base64Payload) {throw new AppError(TokenErrors.INVALID_PAYLOAD);}
   if (!base64Signature) {throw new AppError(TokenErrors.INVALID_SIGNATURE);}
 
-  const expectedSignature = generateSignature(
-    `${base64Header}.${base64Payload}`,
-    secret
-  );
+  const expectedSignature = generateSignature(`${base64Header}.${base64Payload}`);
+
+  console.log('expectedSignature',expectedSignature)
+
   if (base64Signature !== expectedSignature) {
     throw new AppError(TokenErrors.INVALID_SIGNATURE);
   }
-
-  const decoded = decodePayload(token);
-  if (isTokenExpired(decoded)) throw new AppError(TokenErrors.EXPIRED_TOKEN);
 
   return true;
 }
