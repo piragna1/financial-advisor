30040bd8e22a8c064be5b706275c39c1437596d8
piragna <gonzalovarelaalagna@gmail.com>
2025-09-01 20:34:30 -0300

    Debugging

--- src/middlewares/authMiddleware.js
+++ src/middlewares/authMiddleware.js
@@ -1,17 +1,30 @@
-import {extractToken} from '../actors/utils/auth/extractToken.js'
-import {tokenExists} from '../actors/utils/auth/tokenExists.js'
-import { AppError } from '../errors/AppError.js';
-import { TokenErrors } from '../errors/tokenError.js';
-import { verifyToken } from '../utils/auth/verifyToken.js';
+import { extractToken } from "../actors/utils/auth/extractToken.js";
+import { tokenExists } from "../actors/utils/auth/tokenExists.js";
+import { AppError } from "../errors/AppError.js";
+import { TokenErrors } from "../errors/tokenError.js";
+import { verifyToken } from "../utils/auth/verifyToken.js";
+import { decodePayload } from "../utils/auth/token.js";
 
-export function authMiddleware(res,req,next){
-    if (!tokenExists(req)) {
-        throw new AppError(TokenErrors.MISSING_TOKEN);
-    }
-    const token = extractToken(req);
-    const valid = verifyToken(token);
-    if(valid) next();
-    else{
-        throw new AppError(TokenErrors.INVALID_TOKEN);
-    }
+export async function authMiddleware(req, res, next) {
+  if (!tokenExists(req))
+    throw new AppError(TokenErrors.MISSING_TOKEN, "hola que tal");
+
+  const token = extractToken(req);
+  console.log("token", token);
+
+  const valid = await verifyToken(token);
+  console.log("valid", valid);
+
+  const payload = decodePayload(token);
+
+  if (isTokenExpired(payload)) throw new AppError(TokenErrors.EXPIRED_TOKEN);
+
+  console.log("payload", payload);
+  
+  const userId = payload.sub;
+  req.userId = userId;
+  console.log("userId", userId);
+
+  if (valid) next();
+  else throw new AppError(TokenErrors.INVALID_TOKEN);
 }
