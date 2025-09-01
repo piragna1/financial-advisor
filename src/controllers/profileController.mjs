ef3833ef29a17be6093fdb9c40c2ae57e5e66f26
piragna <gonzalovarelaalagna@gmail.com>
2025-09-01 17:33:29 -0300

    fix

--- src/controllers/profileController.mjs
+++ src/controllers/profileController.mjs
@@ -1,13 +1,12 @@
-import { AppError } from "../errors/AppError.js";
-import { ProfileErrors } from "../errors/profileErrors.js";
 import { findUserById } from "../repositories/userRepo.js";
 
-export async function getProfile(req, res) {
-  const userId = req.userId;
-  const user = await findUserById(userId);
-  if (user) {
+export async function getProfile(req, res,next) {
+  try{  
+    const userId = req.params.id;
+    const user = await findUserById(userId);
     res.status(200).json({ user });
-  } else {
-    throw new AppError(ProfileErrors.USER_NOT_FOUND);
+  }catch(err){
+    next(err);
   }
+
 };
