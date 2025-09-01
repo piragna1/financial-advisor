db914c7edb37fdae3f6b8173a7cab408ce76394c
piragna <gonzalovarelaalagna@gmail.com>
2025-09-01 17:33:58 -0300

    Removed logs

--- src/repositories/userRepo.js
+++ src/repositories/userRepo.js
@@ -3,17 +3,10 @@
 import { mockUsers } from '../config/mock.db.config.js';
 
 export async function findUserByEmail(email) {
-  console.log('userRepo.js -> findUserByEmail()')
-  console.log('mockUsers',mockUsers);
-  console.log('mockUsers.find(user => user.email === email)',mockUsers.find(user => user.email === email));
   return mockUsers.find(user => user.email === email);
 }
 
 export async function findUserById(id) {
-  
-  console.log('userRepo.js -> findUserById()')
-  console.log('mockUsers',mockUsers);
-  console.log('mockUsers.find(user => user.id === id)',mockUsers.find(user => user.id === id));
   return mockUsers.find(user => user.id === id);
 }
 
