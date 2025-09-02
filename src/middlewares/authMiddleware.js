ff8b8257fa39b7faa5d08e5d701e6ada7d6d4c8b
piragna <gonzalovarelaalagna@gmail.com>
2025-09-02 12:44:40 -0300

    Debugging logs

--- src/middlewares/authMiddleware.js
+++ src/middlewares/authMiddleware.js
@@ -4,23 +4,24 @@ import { AppError } from "../errors/AppError.js";
 import { TokenErrors } from "../errors/tokenError.js";
 import { verifyToken } from "../utils/auth/verifyToken.js";
 import { decodePayload } from "../utils/auth/token.js";
+import { isTokenExpired } from "../utils/auth/token.js";
 
 export async function authMiddleware(req, res, next) {
-  if (!tokenExists(req))
-    throw new AppError(TokenErrors.MISSING_TOKEN, "hola que tal");
+  if (!tokenExists(req)) throw new AppError(TokenErrors.MISSING_TOKEN);
 
   const token = extractToken(req);
-  console.log("token", token);
+  console.log('extracted token ->>>>',token);
 
   const valid = await verifyToken(token);
   console.log("valid", valid);
 
   const payload = decodePayload(token);
+  console.log('decoded payload:',payload)
 
   if (isTokenExpired(payload)) throw new AppError(TokenErrors.EXPIRED_TOKEN);
 
   console.log("payload", payload);
-  
+
   const userId = payload.sub;
   req.userId = userId;
   console.log("userId", userId);
