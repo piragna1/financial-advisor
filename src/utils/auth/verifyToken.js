584607521a659b843ba2236b40f2d750e8fe1500
piragna <gonzalovarelaalagna@gmail.com>
2025-09-02 12:45:16 -0300

    Modifications that have been done for debugging purposes

--- src/utils/auth/verifyToken.js
+++ src/utils/auth/verifyToken.js
@@ -2,22 +2,22 @@ import { AppError } from "../../errors/AppError.js";
 import {generateSignature} from '../../actors/utils/auth/generateSignature.js'
 import { decodePayload, generateToken, isTokenExpired } from "./token.js";
 import {TokenErrors} from '../../errors/tokenError.js';
+import { jwtConfig } from "../../config/jwtConfig.js";
 
 export async function verifyToken(token) {
 
-  const [base64Header, base64Payload, base64Signature] = token.split(".");
+  const token_arr = token.split(".");
+  const [base64Header, base64Payload, base64Signature] = [token_arr[0].slice(7),token_arr[1],token_arr[2]];
 
-  base64Header.splice(6);
-  console.log('base 64 Header', base64Header)
-  // console.log('base 64 Payload', base64Payload)
-  // console.log('base 64 Signature', base64Signature)
+  console.log('base 64 Header:', base64Header);
+  console.log('base 64 Payload', base64Payload);
+  console.log('base 64 Signature', base64Signature);
   
   if (!base64Header) {throw new AppError(TokenErrors.INVALID_HEADER);}
   if (!base64Payload) {throw new AppError(TokenErrors.INVALID_PAYLOAD);}
   if (!base64Signature) {throw new AppError(TokenErrors.INVALID_SIGNATURE);}
-
-  const expectedSignature = generateSignature(`${base64Header}.${base64Payload}`);
-
+  
+  const expectedSignature = generateSignature(`${base64Header}.${base64Payload}`,jwtConfig.SECRET_SALT);
   console.log('expectedSignature',expectedSignature)
 
   if (base64Signature !== expectedSignature) {
