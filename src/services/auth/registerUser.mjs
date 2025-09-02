d81183c17626e4c2f62a7f1bf8856bab8493a766
piragna <gonzalovarelaalagna@gmail.com>
2025-09-02 19:31:06 -0300

    New import

--- src/services/auth/registerUser.mjs
+++ src/services/auth/registerUser.mjs
@@ -3,6 +3,7 @@ import { findUserByEmail, saveUser } from "../../repositories/userRepo.js";
 import { validateRegistrationInput } from "../../actors/validators/auth/validateRegistrationInput.js";
 import {v4 as uuidv4, v4} from 'uuid';
 import { hashPassword } from "../../utils/auth/hashPassword.js";
+import { passwordSecret } from "../../config/passwordSecretConfig.js";
 
 export async function registerUser({ name,lastName, email, password }) {
   const validation = validateRegistrationInput({
@@ -19,7 +20,7 @@ export async function registerUser({ name,lastName, email, password }) {
     throw new Error("User already exists");
   }
   const id = uuidv4();
-  const passwordHash = hashPassword(password, 'secret');
+  const passwordHash = hashPassword(password, passwordSecret.PASSWORD_SECRET);
   const newUser = buildUserEntity({id,name,lastName,email,passwordHash});
   saveUser(newUser);
   return {success:true, newUser};
