a9a348283d29db5c1dd07e68bac39164b9562d4a
piragna <gonzalovarelaalagna@gmail.com>
2025-09-02 12:44:01 -0300

    Documentation and debugging log

--- src/actors/utils/auth/generateSignature.js
+++ src/actors/utils/auth/generateSignature.js
@@ -1,13 +1,20 @@
-import {createHmac} from 'crypto';
-
-export function generateSignature(string, secret='secret') {
+import { createHmac } from "crypto";
+/**
+ *
+ * @param {*} string The header and the payload of a token formatted as `header.payload`.
+ * @param {*} secret The secret word for salting the token generation.
+ * @returns
+ */
+export function generateSignature(string, secret = "secret") {
   if (typeof string !== "string") {
     throw Error("Invalid input key");
   }
   if (typeof secret !== "string" || secret.length === 0) {
-  throw Error("Invalid secret");
-}
-  const hmac = createHmac('sha256', secret);
+    throw Error("Invalid secret");
+  }
+
+  console.log('Este es el secreto que estoy usando:', secret)
+  const hmac = createHmac("sha256", secret);
   hmac.update(string);
-  return hmac.digest('base64url');
+  return hmac.digest("base64url");
 }
