c41ac3b129f07fe5f3472c572ce595eda72615f7
piragna <gonzalovarelaalagna@gmail.com>
2025-09-02 21:23:37 -0300

    Created function to generate user object

--- src/services/user/user.service.js
+++ src/services/user/user.service.js
@@ -1,6 +1,21 @@
 //user.service.js
-import { saveUser } from "../../repositories/userRepo";
+import { mockUsers } from "../../config/mock.db.config.js";
+import { passwordSecret } from "../../config/passwordSecretConfig.js";
+import { listUsers, saveUser } from "../../repositories/userRepo.js";
+import { hashPassword } from "../../utils/auth/hashPassword.js";
 
 export function persistUser(input){
    saveUser(input);
+}
+
+export function generateUser(name,lastName,email,password){
+   const user = {
+      id:listUsers.length,
+      name,
+      lastName,
+      email,
+      password: hashPassword('sha256', passwordSecret.PASSWORD_SECRET),
+      createdAt: Date.now()
+   };
+   return user;
 }
