473ac90a6e20b13b58016de0d9a62b97d3a7522f
piragna <gonzalovarelaalagna@gmail.com>
2025-09-03 09:10:23 -0300

    Update: -New imports

--- src/services/auth/loginUser.js
+++ src/services/auth/loginUser.js
@@ -1,8 +1,9 @@
 import { userRetrieve } from "../../actors/retrievers/userRetriever.js";
 import { validateLoginInput } from "../../actors/validators/auth/validateLoginInput.js";
+import { passwordSecret } from "../../config/passwordSecretConfig.js";
 import { AppError } from "../../errors/AppError.js";
 import { AuthErrors } from "../../errors/authErrors.js";
-import { comparePasswords } from "../../utils/auth/comparePasswords.js";
+import { comparePasswordHashes } from "../../utils/auth/comparePasswords.js";
 import { issueToken } from "../../utils/auth/tokenIssuer.js";
 
 export async function loginUser({ email, password }) {
@@ -13,7 +14,7 @@ export async function loginUser({ email, password }) {
   const user = await userRetrieve(email);
   if (!user) throw new AppError(AuthErrors.USER_NOT_FOUND);
 
-  const validPass = comparePasswords(user.passwordHash, password);
+  const validPass = comparePasswordHashes(user.passwordHash, password,passwordSecret.PASSWORD_SECRET);
   if (!validPass) throw new AppError(AuthErrors.INVALID_CREDENTIALS);
 
   const token = await issueToken(user);
