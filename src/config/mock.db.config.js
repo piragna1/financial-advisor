ad7a709eec1097b83e8438041f4beb8c25dcf742
piragna <gonzalovarelaalagna@gmail.com>
2025-09-02 21:24:20 -0300

    Mock data modification

--- src/config/mock.db.config.js
+++ src/config/mock.db.config.js
@@ -1,6 +1,6 @@
 //mock.db.config.js
 
 export const mockUsers = [
-  { id: 'u1', email: 'gonzalo@example.com', passwordHash: '8792ba1b8950d6fc15f95bdb8e0ca4c3bfbf01f96d9be49bb200db07a0a40d97' },
+  { id: 'u1', email: 'gonzalo@example.com', passwordHash: '957e81354a1de2c224e92d20f84ef7d535c082995839ccce6b1dc389dc5da509' },
   { id: 'u2', email: 'ana@example.com', passwordHash: '89f529ed4da21cc5c4b253cb9f2abfc65d23a8bb00c382e895f40ca6413bcd09' },
 ];
