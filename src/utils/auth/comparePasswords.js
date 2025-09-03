775c035f033de17bff9681a0b39eea0ae980f386
piragna <gonzalovarelaalagna@gmail.com>
2025-09-03 09:09:42 -0300

    Update function signature

--- src/utils/auth/comparePasswords.js
+++ src/utils/auth/comparePasswords.js
@@ -2,6 +2,6 @@
 import {v4 as uuidv4, v4} from 'uuid';
 import { hashPassword } from './hashPassword.js';
 
-export function comparePasswords(real,input,secret){
+export function comparePasswordHashes(real,input,secret){
     return real === hashPassword(input,secret);
 }
