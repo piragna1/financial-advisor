7ad4669a3502df712d1aeac55fce82d3ce38e00d
piragna <gonzalovarelaalagna@gmail.com>
2025-09-02 21:27:34 -0300

    Deleted function to generate user object since it already exists in entities folder

--- src/services/user/user.service.js
+++ src/services/user/user.service.js
@@ -6,15 +6,3 @@ import { hashPassword } from "../../utils/auth/hashPassword.js";
 export function persistUser(input){
    saveUser(input);
 }
-
-export function generateUser(name,lastName,email,password){
-   const user = {
-      id:listUsers.length,
-      name,
-      lastName,
-      email,
-      password: hashPassword('sha256', passwordSecret.PASSWORD_SECRET),
-      createdAt: Date.now()
-   };
-   return user;
-}
