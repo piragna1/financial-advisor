07ce42b68ea311f99c5720423917adc71046cf5d
piragna <gonzalovarelaalagna@gmail.com>
2025-09-04 17:58:08 -0300

    Adding pure actors to controller and removing them from method

--- src/controllers/authController.mjs
+++ src/controllers/authController.mjs
@@ -2,27 +2,54 @@
 import { findUserByEmail, listUsers } from '../repositories/userRepo.js';
 import { registerUser } from '../services/auth/registerUser.mjs'
 import { loginUser } from '../services/auth/loginUser.js'
+import { validateRegistrationInput } from '../actors/validators/auth/validateRegistrationInput.js';
+import { hashPassword } from '../utils/auth/hashPassword.js';
+import {buildUserEntity} from '../entities/userEntity.js'
+import { generateUserId } from '../services/user/generateUserId.js';
+import { validateLoginInput } from '../actors/validators/auth/validateLoginInput.js';
+import { AuthErrors } from '../errors/authErrors.js';
+import { AppError } from '../errors/AppError.js';
+import { userRetrieve } from '../actors/retrievers/userRetriever.js';
+import { issueToken } from '../utils/auth/tokenIssuer.js';
+import { mapError } from '../errors/mapError.js';
+import { TokenErrors } from '../errors/tokenError.js';
+import { TokenGenerationError } from '../errors/tokenGenerationError.js';
 
 export async function registerUserController(req, res) {
   try {
     const { name, lastName, email, password } = req.body;
-    console.log(`${name} ${lastName}, ${email}, ${password}`)
     
     let newUser = {};
-    await registerUser({ name, lastName,email, password }).then((returnVal) => {
-      newUser = returnVal['newUser'];
-    });
+
+    //call validator
+    if (!validateRegistrationInput({name,lastName,email,password}))throw new Error('Invalid registration input')
+    //call a searcher for seeking user existence
+    let exists = await findUserByEmail(email);
+
+
+    if (exists) throw new AppError();
+
+
+
+    //call a password generator
+    const hashedPassword = hashPassword(password);
+    const id = generateUserId();
+    //call user entity builder
+    newUser = buildUserEntity({id,name,lastName,email,hashedPassword});
+    //call user saver
+    const success = await registerUser(newUser);
+
+
+    if (!success) throw new Error('Error while creating an user')
     
-    const users = listUsers();
-    
+
+
     res.status(200).json({
       message:'User registered',
-      // token,
       user: {
         id: newUser.id,
         email: newUser.email
-      },
-      users
+      }
     });
 
   } catch (error) {
@@ -32,11 +59,32 @@ export async function registerUserController(req, res) {
 
 export async function loginUserController(req, res, next) {
   try {
+    //input
     const {email,password} = req.body;
-    const {user,token,success} = await loginUser({email,password});
-    // return {user, token, success}; debug purposes
+
+    //validate input
+    let valid = validateLoginInput({email, password});
+    if (!valid.ok) throw new AppError (AuthErrors.INVALID_INPUT);
+
+    //userRetrieve
+    let user = await userRetrieve(email);
+    if (!user) throw new AppError(AuthErrors.USER_NOT_FOUND);
+
+    //password hashing comoparing
+    const validPass = comparePasswordHashes(user.passwordHash, password,passwordSecret.PASSWORD_SECRET);
+    if (!validPass) throw new AppError(AuthErrors.INVALID_CREDENTIALS);
+
+    //issue token
+    const token = issueToken(user);
+    if (!token) throw new TokenGenerationError(TokenErrors.TOKEN_GEN_ERROR);
+
+    //return
     res.status(200).json({ user,token,success });
+    
   } catch (error) {
-    next(error);
+
+    const {code, message, status} = mapError(error);
+    res.status(status).json({error:message});
+
   }
 }
--- src/services/auth/registerUser.mjs
+++ src/services/auth/registerUser.mjs
@@ -1,27 +1,8 @@
-import { buildUserEntity } from "../../entities/userEntity.js";
-import { findUserByEmail, saveUser } from "../../repositories/userRepo.js";
-import { validateRegistrationInput } from "../../actors/validators/auth/validateRegistrationInput.js";
-import {v4 as uuidv4, v4} from 'uuid';
-import { hashPassword } from "../../utils/auth/hashPassword.js";
-import { passwordSecret } from "../../config/passwordSecretConfig.js";
+import {  saveUser } from "../../repositories/userRepo.js";
 
-export async function registerUser({ name,lastName, email, password }) {
-  const validation = validateRegistrationInput({
-    name,
-    lastName,
-    email,
-    password,
-  });
-  if (!validation.success) {
-    throw new Error("Invalid input entered");
-  }
-  const exist = await findUserByEmail(email);
-  if (exist) {
-    throw new Error("User already exists");
-  }
-  const id = uuidv4();
-  const passwordHash = hashPassword(password, passwordSecret.PASSWORD_SECRET);
-  const newUser = buildUserEntity({id,name,lastName,email,passwordHash});
-  saveUser(newUser);
-  return {success:true, newUser};
+export async function registerUser(user) {
+  const wasSaved = await saveUser(user);
+  return wasSaved
+    ? { success: true, user }
+    : { success: false, error: 'User persistence failed' };
 }
