119511535fe4ddb4f06507952c2b777153f21942
piragna <gonzalovarelaalagna@gmail.com>
2025-09-05 11:36:13 -0300

    Trying asynchronous execution for debugging

--- src/utils/auth/comparePasswords.js
+++ src/utils/auth/comparePasswords.js
@@ -1,6 +1,9 @@
 //comparePasswords.js
 import { hashPassword } from './hashPassword.js';
 
-export function comparePasswordHashes(real,input,secret){
-    return real === hashPassword(input,secret); //semi pure (crypto import)
+export async function comparePasswordHashes(real,input,secret){
+
+    console.log('comparePasswordHashes()')//debug
+
+    return real === await hashPassword(input,secret); //semi pure (crypto import)
 }
