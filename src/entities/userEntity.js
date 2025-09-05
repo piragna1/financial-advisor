67a0a30282371d1766f45ab4853fc5b01e357512
piragna <gonzalovarelaalagna@gmail.com>
2025-09-05 10:51:05 -0300

    Removed changes that caused errors

--- src/entities/userEntity.js
+++ src/entities/userEntity.js
@@ -1,11 +1,11 @@
 
 export function buildUserEntity({id,name,lastName='',email,hashedPassword}){
     const user = {
-        id:String = id,
-        name:String = name,
-        lastName:String = lastName,
-        email:String = email,
-        hashedPassword:String = hashedPassword
+        id,
+        name,
+        lastName,
+        email,
+        hashedPassword
     }
     return user;
 };
