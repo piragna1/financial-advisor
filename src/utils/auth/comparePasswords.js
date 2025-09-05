bfb7bea6069097143378563d95e8e9ca5d3b2a41
piragna <gonzalovarelaalagna@gmail.com>
2025-09-05 11:40:09 -0300

    Debug

--- src/utils/auth/comparePasswords.js
+++ src/utils/auth/comparePasswords.js
@@ -4,6 +4,10 @@ import { hashPassword } from './hashPassword.js';
 export async function comparePasswordHashes(real,input,secret){
 
     console.log('comparePasswordHashes()')//debug
+    console.log('real password', real)
+    console.log('input password', input)
+    console.log('secret', secret)
+
 
     return real === await hashPassword(input,secret); //semi pure (crypto import)
 }
