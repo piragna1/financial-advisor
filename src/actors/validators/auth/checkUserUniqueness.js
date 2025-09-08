0a4cb9a3651127508154a3f6a013731151a74f81
piragna <gonzalovarelaalagna@gmail.com>
2025-09-08 10:58:44 -0300

    checkUserUniqueness working

--- src/actors/validators/auth/checkUserUniqueness.js
+++ src/actors/validators/auth/checkUserUniqueness.js
@@ -1,5 +1,5 @@
 export async function checkUserUniqueness (email, users) {
-    const exist = users.some((user) => u.email === email);
+    const exist = users.some((user) => user['email'] === email);
     return {
         succes: exist,
         message: exist ? 'Email is not valid' : 'Email available'
@@ -7,7 +7,9 @@ export async function checkUserUniqueness (email, users) {
 }
 
 //mock.db.config.js
-export const users = [
+const users = [
   { id: 'u1', email: 'gonzalo@example.com', passwordHash: '985b883e66e9f181eede78d83eca52c67f6e571b82599d1c1da9681920e007df' },
   { id: 'u2', email: 'ana@example.com', passwordHash: '1612919b91393fdab5c16a4a979b54f90b2870afa0772f26a1919d5e41fa7996' },
 ];
+console.log(await checkUserUniqueness('gonzalo@ecample.com',users));//
+console.log(await checkUserUniqueness('gonzalo@example.com',users));//
