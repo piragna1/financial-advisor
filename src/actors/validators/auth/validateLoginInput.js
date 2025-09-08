f9bfd5cb64b01c77a896bb783468cee3e88b7e8d
piragna <gonzalovarelaalagna@gmail.com>
2025-09-08 11:35:16 -0300

    Working on validateLoginInput()

--- src/actors/validators/auth/validateLoginInput.js
+++ src/actors/validators/auth/validateLoginInput.js
@@ -16,3 +16,34 @@ export function validateLoginInput(input) {
     ? { ok: true, value: { email, password } }
     : { ok: false, value: errors };
 }
+//------------------------------------- TEST CASES
+
+//-----Works fine
+console.log(validateLoginInput({email:'gonzalo@.com', password:'lalala123'}));//{ ok: false, value: { email: 'Email format is 
+// invalid' } }
+
+console.log(validateLoginInput({email:'gonzalo@example.com', password:'lalala123'}));/*{
+  ok: true,
+  value: { email: 'gonzalo@example.com', password: 'lalala123' }
+} */
+
+
+console.log(validateLoginInput({ email: 12345, password: 'securePass123' }));//{ ok: false, value: { email: 'Email must be a string.' } }
+//-------------------------------------
+
+//-----Must correct
+console.log(validateLoginInput({ password: 'securePass123' }))//{ ok: false, value: { email: 'Email must be a string.' } } should be: email is required
+console.log(validateLoginInput({email:'gonzalo@example.com', password:123}));/* 
+{
+  ok: false,
+  value: { password: 'Password is required and must be a string' }
+}
+Should be: password must be a string  
+*/
+console.log(validateLoginInput({ email: 'user@example.com' }))/* {
+  ok: false,
+  value: { password: 'Password is required and must be a string' }
+}
+should be: password is required  
+*/
+
