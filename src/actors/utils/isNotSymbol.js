f2159e7049e159cb13a465aeda8f03062a314c0d
piragna <gonzalovarelaalagna@gmail.com>
2025-09-08 15:55:33 -0300

    Testing new isNotSymbol method

--- src/actors/utils/isNotSymbol.js
+++ src/actors/utils/isNotSymbol.js
@@ -2,3 +2,11 @@ export function isNotSymbol(char){
     // Check if the character is alphanumeric or a space
   return /^[a-zA-Z0-9 ]$/.test(char);
 }
+
+console.log(isNotSymbol());//f
+console.log(isNotSymbol(''));//f
+console.log(isNotSymbol('1'));//t
+console.log(isNotSymbol('A'));//t
+console.log(isNotSymbol('a'));//t
+console.log(isNotSymbol(' '));//t
+console.log(isNotSymbol('$'));//f
