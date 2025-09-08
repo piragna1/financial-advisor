2b82a4647984538f5b1051255f8bdb028fef5bf4
piragna <gonzalovarelaalagna@gmail.com>
2025-09-08 12:09:45 -0300

    Update function's error handling

--- src/actors/validators/auth/validateLoginInput.js
+++ src/actors/validators/auth/validateLoginInput.js
@@ -1,16 +1,24 @@
 //validateLoginInput.js
 export function validateLoginInput(input) {
   const { email, password } = input;
-  const errors = {};
-  if (!email) errors.email = "required";
-  if (!password) errors.password = "required";
-  if (!email || typeof email !== "string") {
-    errors.email = "Email must be a string.";
+  const errors = {
+    email:[],
+    password:[]
+  };
+  if (!email) {
+    errors.email.push("Email isrequired");
+  }
+  if (!password){ errors.password.push("Password is required.");
+
+  };
+  
+  if (typeof email !== "string") {
+    errors.email.push("Email must be a string.");
   } else if (!/^\S+@\S+\.\S+$/.test(email)) {
-    errors.email = "Email format is invalid";
+    errors.email.push("Email format is invalid");
   }
   if (!password || typeof password !== "string") {
-    errors.password = "Password is required and must be a string";
+    errors.password.push("Password is required and must be a string");
   }
   return Object.keys(errors).length === 0
     ? { ok: true, value: { email, password } }
@@ -21,25 +29,29 @@ export function validateLoginInput(input) {
 //-----Works fine
 
 //--invalid email format
-console.log(validateLoginInput({email:'gonzalo@.com', password:'lalala123'}));//{ ok: false, value: { email: 'Email format is 
+console.log(
+  validateLoginInput({ email: "gonzalo@.com", password: "lalala123" })
+); //{ ok: false, value: { email: 'Email format is
 // invalid' } }
 
 //correct input
-console.log(validateLoginInput({email:'gonzalo@example.com', password:'lalala123'}));/*{
+console.log(
+  validateLoginInput({ email: "gonzalo@example.com", password: "lalala123" })
+); /*{
   ok: true,
   value: { email: 'gonzalo@example.com', password: 'lalala123' }
 } */
 
 //email not string
-console.log(validateLoginInput({ email: 12345, password: 'securePass123' }));//{ ok: false, value: { email: 'Email must be a string.' } }
+console.log(validateLoginInput({ email: 12345, password: "securePass123" })); //{ ok: false, value: { email: 'Email must be a string.' } }
 //-------------------------------------
 
 //-----Must correct
 //missing email
-console.log(validateLoginInput({ password: 'securePass123' }))//{ ok: false, value: { email: 'Email must be a string.' } } should be: email is required
+console.log(validateLoginInput({ password: "securePass123" })); //{ ok: false, value: { email: 'Email must be a string.' } } should be: email is required
 
 //missing password
-console.log(validateLoginInput({ email: 'user@example.com' }))/* {
+console.log(validateLoginInput({ email: "user@example.com" })); /* {
   ok: false,
   value: { password: 'Password is required and must be a string' }
 }
@@ -47,7 +59,9 @@ should be: password is required
 */
 
 //password not string
-console.log(validateLoginInput({email:'gonzalo@example.com', password:123}));/* 
+console.log(
+  validateLoginInput({ email: "gonzalo@example.com", password: 123 })
+); /* 
 {
   ok: false,
   value: { password: 'Password is required and must be a string' }
@@ -55,15 +69,34 @@ console.log(validateLoginInput({email:'gonzalo@example.com', password:123}));/*
 Should be: password must be a string  
 */
 
-//-------------------------------------
-
 //Missing email and missing password
-console.log(validateLoginInput({}));/* {
+console.log(validateLoginInput({})); /* {
   ok: false,
   value: {
     email: 'Email must be a string.',
     password: 'Password is required and must be a string'
   }
 }
-  
+  expected:{
+    ok: false,
+    value: {
+      email: 'Email is required.',
+      password: 'Password is required'
+    }
+  }
 */
+
+//Empty strings
+console.log(validateLoginInput({ email: "", password: "" })); //expected:
+// // → { ok: false, value: { email: 'Email format is invalid', password: 'Password is required and must be a string' } }
+//output:
+/*
+{
+  ok: false,
+  value: {
+    email: 'Email must be a string.',
+    password: 'Password is required and must be a string'  
+*/
+
+//-------------------------------------
+
