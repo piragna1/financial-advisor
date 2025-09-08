ee7a0d813394846568ed95147af58ebbc4aed38b
piragna <gonzalovarelaalagna@gmail.com>
2025-09-08 15:06:10 -0300

    Test casses passed succesfully

--- src/actors/validators/auth/validateLoginInput.js
+++ src/actors/validators/auth/validateLoginInput.js
@@ -9,31 +9,3 @@ export function validateLoginInput(input) {
   const errors = {email:emailErrors, password:passwordErrors}
   return errors;
 }
-//------------------------------------- TEST CASES
-console.log('invalid email format');
-console.log(
-  validateLoginInput({ email: "gonzalo@.com", password: "lalala123" }) 
-); 
-
-console.log('correct input')
-console.log(
-  validateLoginInput({ email: "gonzalo@example.com", password: "lalala123" })
-); 
-
-console.log('email not string')
-console.log(validateLoginInput({ email: 12345, password: "securePass123" }));
-console.log('missing email')
-console.log(validateLoginInput({ password: "securePass123" })); 
-console.log('missing password')
-console.log(validateLoginInput({ email: "user@example.com" })); 
-console.log('password not string')
-console.log(
-  validateLoginInput({ email: "gonzalo@example.com", password: 123 })
-); 
-console.log('Missing email and missing password')
-console.log(validateLoginInput({}));
-console.log('Empty strings')
-console.log(validateLoginInput({ email: "", password: "" })); 
-// //-------------------------------------
-
-
