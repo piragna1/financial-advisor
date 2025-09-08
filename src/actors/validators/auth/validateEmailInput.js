6e6422bded657236eb491286ba44cba6e835a90f
piragna <gonzalovarelaalagna@gmail.com>
2025-09-08 14:28:14 -0300

    Fixing

--- src/actors/validators/auth/validateEmailInput.js
+++ src/actors/validators/auth/validateEmailInput.js
@@ -1,13 +1,13 @@
 export function validateEmailInput(email) {
   const errors = [];
-
   if (!email) {
-    errors.push("Email is required");
-  } else if (typeof email !== "string") {
-    errors.push("Email must be a string.");
-  } else if (!/^\S+@\S+\.\S+$/.test(email)) {
-    errors.push("Email format is invalid");
+    errors.push("Email isrequired");
+  } else {
+    if (email && typeof email !== "string") {
+      errors.push("Email must be a string.");
+    } else if (email && !/^\S+@\S+\.\S+$/.test(email)) {
+      errors.push("Email format is invalid");
+    }
   }
-
   return errors;
 }
