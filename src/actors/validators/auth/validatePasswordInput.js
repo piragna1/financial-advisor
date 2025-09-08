149a0fa5f73bce5800570a4296e2d44eefa4eb15
piragna <gonzalovarelaalagna@gmail.com>
2025-09-08 14:20:48 -0300

    Password validation first implementation

--- src/actors/validators/auth/validatePasswordInput.js
+++ src/actors/validators/auth/validatePasswordInput.js
@@ -1,1 +1,7 @@
-export function validatePasswordInput(errors,password){}
+export function validatePasswordInput(password){
+    const errors = {};
+    if (!password){ errors.password.push("Password is required."); };
+    if (password && typeof password !== "string") { errors.password.push("Password is required and must be a string"); }
+
+    return errors;
+}
