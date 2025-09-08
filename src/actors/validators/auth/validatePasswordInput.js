13561ae76c684de8e2377c870fcd874f5df6a36a
piragna <gonzalovarelaalagna@gmail.com>
2025-09-08 15:01:18 -0300

    Function fixed and optimized

--- src/actors/validators/auth/validatePasswordInput.js
+++ src/actors/validators/auth/validatePasswordInput.js
@@ -1,7 +1,9 @@
 export function validatePasswordInput(password){
-    const errors = {};
-    if (!password){ errors.password.push("Password is required."); };
-    if (password && typeof password !== "string") { errors.password.push("Password is required and must be a string"); }
-
+    const errors = [];
+    if (!password){ errors.push("Password is required."); }
+    else{
+        if (typeof password !== "string") { errors.push("Password must be a string"); }
+        else if (password.length <= 3) errors.push('Password is too short!');
+    }
     return errors;
 }
