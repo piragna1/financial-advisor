bad5dce9ef4e3c45e8039f7f213dc2b6535af513
piragna <gonzalovarelaalagna@gmail.com>
2025-09-08 14:26:44 -0300

    Fixing

--- src/actors/validators/auth/validateEmailInput.js
+++ src/actors/validators/auth/validateEmailInput.js
@@ -1,12 +1,13 @@
 export function validateEmailInput(email) {
-    const errors = [];
+  const errors = [];
+
   if (!email) {
-    errors.push("Email isrequired");
-  }
-  if (email && typeof email !== "string") {
+    errors.push("Email is required");
+  } else if (typeof email !== "string") {
     errors.push("Email must be a string.");
-  } else if (email && !/^\S+@\S+\.\S+$/.test(email)) {
+  } else if (!/^\S+@\S+\.\S+$/.test(email)) {
     errors.push("Email format is invalid");
   }
-}
 
+  return errors;
+}
