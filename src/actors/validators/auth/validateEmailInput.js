1e026f9e3544f8e7ee1b3e919353771d00318d94
piragna <gonzalovarelaalagna@gmail.com>
2025-09-08 14:24:56 -0300

    Missing bracket

--- src/actors/validators/auth/validateEmailInput.js
+++ src/actors/validators/auth/validateEmailInput.js
@@ -3,7 +3,6 @@ export function validateEmailInput(email) {
   if (!email) {
     errors.email.push("Email isrequired");
   }word.push("Password is required.");
-  }
   if (email && typeof email !== "string") {
     errors.email.push("Email must be a string.");
   } else if (email && !/^\S+@\S+\.\S+$/.test(email)) {
