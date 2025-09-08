7a754e69c5b1a33a9bbcef85632bf02d903d879b
piragna <gonzalovarelaalagna@gmail.com>
2025-09-08 11:40:51 -0300

    Working on validateLoginInput.js

--- src/actors/validators/auth/validateLoginInput.js
+++ src/actors/validators/auth/validateLoginInput.js
@@ -19,27 +19,26 @@ export function validateLoginInput(input) {
 //------------------------------------- TEST CASES
 
 //-----Works fine
+
+//--invalid email format
 console.log(validateLoginInput({email:'gonzalo@.com', password:'lalala123'}));//{ ok: false, value: { email: 'Email format is 
 // invalid' } }
 
+//correct input
 console.log(validateLoginInput({email:'gonzalo@example.com', password:'lalala123'}));/*{
   ok: true,
   value: { email: 'gonzalo@example.com', password: 'lalala123' }
 } */
 
-
+//email not string
 console.log(validateLoginInput({ email: 12345, password: 'securePass123' }));//{ ok: false, value: { email: 'Email must be a string.' } }
 //-------------------------------------
 
 //-----Must correct
+//missing email
 console.log(validateLoginInput({ password: 'securePass123' }))//{ ok: false, value: { email: 'Email must be a string.' } } should be: email is required
-console.log(validateLoginInput({email:'gonzalo@example.com', password:123}));/* 
-{
-  ok: false,
-  value: { password: 'Password is required and must be a string' }
-}
-Should be: password must be a string  
-*/
+
+//missing password
 console.log(validateLoginInput({ email: 'user@example.com' }))/* {
   ok: false,
   value: { password: 'Password is required and must be a string' }
@@ -47,3 +46,24 @@ console.log(validateLoginInput({ email: 'user@example.com' }))/* {
 should be: password is required  
 */
 
+//password not string
+console.log(validateLoginInput({email:'gonzalo@example.com', password:123}));/* 
+{
+  ok: false,
+  value: { password: 'Password is required and must be a string' }
+}
+Should be: password must be a string  
+*/
+
+//-------------------------------------
+
+//Missing email and missing password
+console.log(validateLoginInput({}));/* {
+  ok: false,
+  value: {
+    email: 'Email must be a string.',
+    password: 'Password is required and must be a string'
+  }
+}
+  
+*/
