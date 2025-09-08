7352c06b0049416741129985750ce0e5388945a0
piragna <gonzalovarelaalagna@gmail.com>
2025-09-08 14:26:25 -0300

    Fixing

--- src/actors/validators/auth/validateEmailInput.js
+++ src/actors/validators/auth/validateEmailInput.js
@@ -2,7 +2,7 @@ export function validateEmailInput(email) {
     const errors = [];
   if (!email) {
     errors.push("Email isrequired");
-  }word.push("Password is required.");
+  }
   if (email && typeof email !== "string") {
     errors.push("Email must be a string.");
   } else if (email && !/^\S+@\S+\.\S+$/.test(email)) {
