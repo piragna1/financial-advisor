21a7fff8644d1c0200422ecbe23c416d7f2c528d
piragna <gonzalovarelaalagna@gmail.com>
2025-09-08 14:20:38 -0300

    Email validation implementation

--- src/actors/validators/auth/validateEmailInput.js
+++ src/actors/validators/auth/validateEmailInput.js
@@ -1,2 +1,12 @@
-export function validateEmailInput(errors, email){
+export function validateEmailInput(email) {
+    const errors = {};
+  if (!email) {
+    errors.email.push("Email isrequired");
+  }word.push("Password is required.");
+  }
+  if (email && typeof email !== "string") {
+    errors.email.push("Email must be a string.");
+  } else if (email && !/^\S+@\S+\.\S+$/.test(email)) {
+    errors.email.push("Email format is invalid");
+  }
 }
