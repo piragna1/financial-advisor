fe2b58b10e520d8ac063d7e9b6f329e182a2cf69
piragna <gonzalovarelaalagna@gmail.com>
2025-09-09 10:11:06 -0300

    Modified validateName implementation

--- src/actors/validators/auth/validateName.js
+++ src/actors/validators/auth/validateName.js
@@ -1,26 +1,25 @@
+import { AppError } from '../../../errors/AppError.js';
+import { AuthErrors } from '../../../errors/authErrors.js';
 import {isNotSymbol} from '../../utils/isNotSymbol.js'
 export function validateName(name){
-    const errors = [];
     if (!name){
-        errors.push('Name is missing')
+        throw new AppError(AuthErrors.MISSING_CREDENTIALS, 'Name is missing')
     }
     else {
-        if (name.length < 3 || name.length > 35) {
-            errors.push('The length of the name is not valid')
+        if (name.length < 3 || name.length > 35 ) {
+        throw new AppError(AuthErrors.INVALID_INPUT, 'Name is too short (or too long)');
         }
         else if (typeof name === 'number'){
-            errors.push('The must be a string')
+        throw new AppError(AuthErrors.INVALID_INPUT, 'Name must be a string');
         }
         for (const char of name) {
             if (!isNaN(char)) {
-                errors.push('The name cannot contain characters!')
-                break;
+                throw new AppError(AuthErrors.INVALID_INPUT, 'Name cannot contain numbers');
             }
             if (!isNotSymbol(char)){
-                errors.push('The name cannot contain symbols!')
-                break;
+                throw new AppError(AuthErrors.INVALID_INPUT, 'Name cannot contain symbols');
             }
         }
     }
-    return errors;
+    return true;
 }
