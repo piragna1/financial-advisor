8e93dbc11f34b312642c8e109106f7dcbf361518
piragna <gonzalovarelaalagna@gmail.com>
2025-09-09 10:26:55 -0300

    validatePasswordInput(): -Adding new case for throwing an error (more than 200 characters) -Adding details too each error case

--- src/actors/validators/auth/validatePasswordInput.js
+++ src/actors/validators/auth/validatePasswordInput.js
@@ -3,13 +3,14 @@ import { AuthErrors } from "../../../errors/authErrors";
 
 export function validatePasswordInput(password){
     if (!password){ 
-        throw new AppError(AuthErrors.MISSING_CREDENTIALS);
+        throw new AppError(AuthErrors.MISSING_CREDENTIALS, 'Password is missing');
     }
     else{
         if (typeof password !== "string") {
-            throw new AppError(AuthErrors.INVALID_INPUT);
+            throw new AppError(AuthErrors.INVALID_INPUT, 'Password must be a string');
         }
-        else if (password.length <= 3) throw new AppError(AuthErrors.INVALID_INPUT);
+        else if (password.length <= 3) throw new AppError(AuthErrors.INVALID_INPUT, 'Password must contain more than 3 characters');
+        else if (password.length >=200) throw new AppError (AuthErrors.INVALID_INPUT, 'Password must be less than 200 characters');
     }
     return true;
 }
