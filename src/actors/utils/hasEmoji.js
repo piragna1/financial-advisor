553dd1acb423c01181e636f5021e8adb4af9b0d8
piragna <gonzalovarelaalagna@gmail.com>
2025-09-09 14:47:32 -0300

    FILE  hasEmoji.js: - Modified regular expression for avoiding false positives in a more eficcient, proper way

--- src/actors/utils/hasEmoji.js
+++ src/actors/utils/hasEmoji.js
@@ -1,4 +1,4 @@
 export function hasEmoji(input){
-    console.log('input:',input) //debug purposes
-    return /[\p{Emoji}]/gu.test(input);
+    const emojiRegex =/[\u{1F600}-\u{1F64F}]|[\u{1F300}-\u{1F5FF}]|[\u{1F680}-\u{1F6FF}]|[\u{1F1E6}-\u{1F1FF}]|[\u{2600}-\u{26FF}]|[\u{2700}-\u{27BF}]/u;
+    return emojiRegex.test(input);
 }
