01c1c10825173f2fa08aee4c9962ab07f63641a6
piragna <gonzalovarelaalagna@gmail.com>
2025-09-09 08:57:09 -0300

    Test case passed: valid input

--- src/actors/validators/auth/validateRegistrationInput.js
+++ src/actors/validators/auth/validateRegistrationInput.js
@@ -22,89 +22,96 @@ export function validateRegistrationInput({ name,lastName, email, password }) {
 
 //----------TEST CASES
 //✅ Valid Input
-validateRegistrationInput({
-  name: "Gonzalo",
-  lastName: "Varela",
-  email: "gonzalo@example.com",
-  password: "securePass123"
-});
+console.log(
+  validateRegistrationInput({
+    name: "Gonzalo",
+    lastName: "Varela",
+    email: "gonzalo@example.com",
+    password: "securePass123"
+  })
+);
 // → returns true
 
-//❌ Missing Fields
-validateRegistrationInput({});
-// → throws Error with:
-// {
-//   name: ["Name is required"],
-  //  lastName: ["Last name is required"],
-  //  email: ["Email is required"],
-  //  password: ["Password is required"]
+// //❌ Missing Fields
+// try {
+  
+//   validateRegistrationInput({});
+// } catch (error) {
+//   console.error(error)
 // }
+// // → throws Error with:
+// // {
+// //   name: ["Name is required"],
+//   //  lastName: ["Last name is required"],
+//   //  email: ["Email is required"],
+//   //  password: ["Password is required"]
+// // }
 
 
-// ❌ Invalid Email Format
-validateRegistrationInput({
-  name: "Gonzalo",
-  lastName: "Varela",
-  email: "gonzalo@.com",
-  password: "securePass123"
-});
-// → throws Error with: { email: ["Email format is invalid"] }
+// // ❌ Invalid Email Format
+// validateRegistrationInput({
+//   name: "Gonzalo",
+//   lastName: "Varela",
+//   email: "gonzalo@.com",
+//   password: "securePass123"
+// });
+// // → throws Error with: { email: ["Email format is invalid"] }
 
-//❌ Email Not a String
-validateRegistrationInput({
-  name: "Gonzalo",
-  lastName: "Varela",
-  email: 12345,
-  password: "securePass123"
-});
-// → throws Error with: { email: ["Email must be a string."] }
-
-
-//❌ Password Not a String
-validateRegistrationInput({
-  name: "Gonzalo",
-  lastName: "Varela",
-  email: "gonzalo@example.com",
-  password: 12345
-});
-// → throws Error with: { password: ["Password must be a string."] }
+// //❌ Email Not a String
+// validateRegistrationInput({
+//   name: "Gonzalo",
+//   lastName: "Varela",
+//   email: 12345,
+//   password: "securePass123"
+// });
+// // → throws Error with: { email: ["Email must be a string."] }
 
 
-//❌ Empty Strings
-validateRegistrationInput({
-  name: "",
-  lastName: "",
-  email: "",
-  password: ""
-});
-// → throws Error with:
-// {
-//   name: ["Name is required"],
-  //  lastName: ["Last name is required"],
-  //  email: ["Email format is invalid"],
-  //  password: ["Password must be a string"]
-// }
+// //❌ Password Not a String
+// validateRegistrationInput({
+//   name: "Gonzalo",
+//   lastName: "Varela",
+//   email: "gonzalo@example.com",
+//   password: 12345
+// });
+// // → throws Error with: { password: ["Password must be a string."] }
 
 
-//❌ Name Not a String
-validateRegistrationInput({
-  name: 123,
-  lastName: "Varela",
-  email: "gonzalo@example.com",
-  password: "securePass123"
-});
-// → throws Error with: { name: ["Name must be a string."] }
+// //❌ Empty Strings
+// validateRegistrationInput({
+//   name: "",
+//   lastName: "",
+//   email: "",
+//   password: ""
+// });
+// // → throws Error with:
+// // {
+// //   name: ["Name is required"],
+//   //  lastName: ["Last name is required"],
+//   //  email: ["Email format is invalid"],
+//   //  password: ["Password must be a string"]
+// // }
 
 
-//❌ Last Name Not a String
+// //❌ Name Not a String
+// validateRegistrationInput({
+//   name: 123,
+//   lastName: "Varela",
+//   email: "gonzalo@example.com",
+//   password: "securePass123"
+// });
+// // → throws Error with: { name: ["Name must be a string."] }
 
-validateRegistrationInput({
-  name: "Gonzalo",
-  lastName: 456,
-  email: "gonzalo@example.com",
-  password: "securePass123"
-});
-// → throws Error with: { lastName: ["Last name must be a string."] }
+
+// //❌ Last Name Not a String
+
+// validateRegistrationInput({
+//   name: "Gonzalo",
+//   lastName: 456,
+//   email: "gonzalo@example.com",
+//   password: "securePass123"
+// });
+// // → throws Error with: { lastName: ["Last name must be a string."] }
 
 
 
