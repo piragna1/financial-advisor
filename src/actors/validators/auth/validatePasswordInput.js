e8ad44e6f5906e0120c1d6ff074585ecdf42d401
piragna <gonzalovarelaalagna@gmail.com>
2025-09-09 13:37:31 -0300

    Update validatePasswordInput: - New import - New implementation for the case: password with control characters

--- src/actors/validators/auth/validatePasswordInput.js
+++ src/actors/validators/auth/validatePasswordInput.js
@@ -2,6 +2,7 @@ import { AppError } from "../../../errors/AppError.js";
 import { AuthErrors } from "../../../errors/authErrors.js";
 import {hasEmoji} from "../../utils/hasEmoji.js"
 import { hasWhiteSpaces } from "../../utils/hasWhiteSpaces.js";
+import {hasControlChars} from '../../utils/hasControlChars.js'
 
 export function validatePasswordInput(password){
     if (!password){ 
@@ -16,6 +17,7 @@ export function validatePasswordInput(password){
           else if (password.length <= 3) throw new AppError(AuthErrors.INVALID_INPUT, 'Password must contain more than 3 characters');
           else if (password.length >=200) throw new AppError (AuthErrors.INVALID_INPUT, 'Password must be less than 200 characters');
           else if (hasEmoji(password)) throw new AppError(AuthErrors.INVALID_INPUT, 'The password cannot contain emojis')
+          else if (hasControlChars(password)) throw new AppError(AuthErrors.INVALID_INPUT, 'There must not be any control characters in password')
       }
     }
     return true;
