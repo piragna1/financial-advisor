df0f49a40a4e4b34a41fe63a26c901660efd2382
piragna <gonzalovarelaalagna@gmail.com>
2025-09-09 09:29:56 -0300

    New auth type error added

--- src/errors/authErrors.js
+++ src/errors/authErrors.js
@@ -4,15 +4,20 @@ export const AuthErrors= {
         message:"Invalid email or password format",
         status:400
     },
-    USER_NOT_FOUND:{
-        code:"USER_NOT_FOUND",
-        message:"User not found",
-        status:404,
-    },
     INVALID_CREDENTIALS:{
         code:"INVALID_CREDENTIALS",
         message:"Invalid email or password",
         status:401
+    },
+    MISSING_CREDENTIALS:{
+        code:"MISSING_CREDENTIALS",
+        message:"Email or password is missing",
+        status:402
+    },
+    USER_NOT_FOUND:{
+        code:"USER_NOT_FOUND",
+        message:"User not found",
+        status:404,
     },
     USER_EXISTS:{
         code:"USER_ALREADY_EXISTS",
