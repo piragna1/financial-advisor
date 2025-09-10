0562da770b8f44af13ba625ba326f4e22f67e44e
piragna <gonzalovarelaalagna@gmail.com>
2025-09-10 12:01:59 -0300

    Removed unused invalid register entries

--- src/config/registerData.js
+++ src/config/registerData.js
@@ -4,29 +4,3 @@ export const validRegisterInput = {
   firstName: 'Gonzalo',
   lastName: 'Rodríguez',
 };
-
-export const invalidRegisterInputs = {
-  missingEmail: {
-    password: 'SuperSecure123!',
-    firstName: 'Gonzalo',
-    lastName: 'Rodríguez',
-  },
-  shortPassword: {
-    email: 'gonzalo@example.com',
-    password: '123',
-    firstName: 'Gonzalo',
-    lastName: 'Rodríguez',
-  },
-  nameWithNumbers: {
-    email: 'gonzalo@example.com',
-    password: 'SuperSecure123!',
-    firstName: 'G0nzalo',
-    lastName: 'Rodríguez2',
-  },
-  emptyFields: {
-    email: '',
-    password: '',
-    firstName: '',
-    lastName: '',
-  },
-};
