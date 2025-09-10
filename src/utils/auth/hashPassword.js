aa4f3c0edb34405dac8a2d0b842a5ed65c9cee39
piragna <gonzalovarelaalagna@gmail.com>
2025-09-10 12:35:09 -0300

    Removed log

--- src/utils/auth/hashPassword.js
+++ src/utils/auth/hashPassword.js
@@ -2,6 +2,5 @@
 import { createHmac } from 'crypto';
 
 export function hashPassword(input,secret='secret'){
-    console.log('hashPassword() ->{}')
     return createHmac('sha256',secret).update(input).digest('hex');
 }
