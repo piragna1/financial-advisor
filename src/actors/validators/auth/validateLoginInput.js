5655af1418d7767a7cdcaedbbbf029d08ecec99a
piragna <gonzalovarelaalagna@gmail.com>
2025-09-10 15:55:51 -0300

    Marking methods as checked

--- src/actors/validators/auth/validateLoginInput.js
+++ src/actors/validators/auth/validateLoginInput.js
@@ -4,8 +4,8 @@ import { validatePasswordInput } from "./validatePasswordInput.js";
 //validateLoginInput.js
 export function validateLoginInput(input) {
   const { email, password } = input;
-  const emailErrors = validateEmailInput(email);
-  const passwordErrors = validatePasswordInput(password);
+  const emailErrors = validateEmailInput(email);//checked (see backlog.txt)
+  const passwordErrors = validatePasswordInput(password);//checked (see backlog.txt)
   return true;
 }
 
