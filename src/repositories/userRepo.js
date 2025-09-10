111a4cec0088316d8633c8425b87523fc06b15ad
piragna <gonzalovarelaalagna@gmail.com>
2025-09-10 15:14:01 -0300

    🗃️ Formatted document

--- src/repositories/userRepo.js
+++ src/repositories/userRepo.js
@@ -1,13 +1,13 @@
 // userRepo.js
 
-import { mockUsers } from '../config/mock.db.config.js';
+import { mockUsers } from "../config/mock.db.config.js";
 
 export async function findUserByEmail(email) {
-  return mockUsers.find(user => user.email === email);
+  return mockUsers.find((user) => user.email === email);
 }
 
 export async function findUserById(id) {
-  return mockUsers.find(user => user.id === id);
+  return mockUsers.find((user) => user.id === id);
 }
 
 export function saveUser(user) {
@@ -15,14 +15,14 @@ export function saveUser(user) {
   return user;
 }
 
-export function listUsers(){
+export function listUsers() {
   return mockUsers.slice();
 }
 
-export function deleteUser(id){
-  for (let i = 0; i < mockUsers.length; i++){
-    if (mockUsers[i].id === id){
-      mockUsers.splice(i,1)
+export function deleteUser(id) {
+  for (let i = 0; i < mockUsers.length; i++) {
+    if (mockUsers[i].id === id) {
+      mockUsers.splice(i, 1);
       return;
     }
   }
