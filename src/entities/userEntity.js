d07fd3bf4ec531cac5eef4c60c82a977f80e7be7
piragna <gonzalovarelaalagna@gmail.com>
2025-09-10 13:46:46 -0300

    🔁 - Added missing 'createdAt' field to the user entity builder - Setting test case 🧪

--- src/entities/userEntity.js
+++ src/entities/userEntity.js
@@ -5,7 +5,18 @@ export function buildUserEntity({id,name,lastName='',email,hashedPassword}){
         name,
         lastName,
         email,
-        hashedPassword
+        hashedPassword,
+        createdAt:Date.now()
     }
     return user;
 };
+
+console.log(buildUserEntity(
+    {
+        id:'1',
+        name:'gon',
+        lastName:'var',
+        email:'gonzalo@example.com',
+        hashedPassword:'7cfb26784163a421daa0dee7c46de2e8d0c825223ed2ff07ce3069682c3d9a12'
+    }
+));
