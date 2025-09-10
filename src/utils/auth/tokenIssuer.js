aaab754fe5329c850cca9975296a1b3969986a31
piragna <gonzalovarelaalagna@gmail.com>
2025-09-10 16:27:41 -0300

    removed debug log

--- src/utils/auth/tokenIssuer.js
+++ src/utils/auth/tokenIssuer.js
@@ -2,7 +2,6 @@ import { jwtConfig } from '../../config/jwtConfig.js';
 import {generateToken} from '../../utils/auth/token.js'
 //tokenIssuer.js
 export function issueToken(user){
-    console.log('issueToken()')
     //default value for secret and expiresInSeconds
     return generateToken(user.id, jwtConfig.SECRET_SALT, 3600);
 }
