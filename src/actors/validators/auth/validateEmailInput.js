bd02f558b306354a057a1100b3ff3f54ca42768d
piragna <gonzalovarelaalagna@gmail.com>
2025-09-10 10:33:02 -0300

    FILE - validateEmailInput.js: - Changed error message

--- src/actors/validators/auth/validateEmailInput.js
+++ src/actors/validators/auth/validateEmailInput.js
@@ -6,7 +6,7 @@ export function validateEmailInput(email) {
     throw new AppError(AuthErrors.MISSING_CREDENTIALS, 'Email is required');
   } else {
     if (email && typeof email !== "string") {
-      throw new AppError(AuthErrors.INVALID_INPUT, 'Email cannot be a number')
+      throw new AppError(AuthErrors.INVALID_INPUT, 'Email must be a string')
     } else if (email && !/^\S+@\S+\.\S+$/.test(email)) {
       throw new AppError(AuthErrors.INVALID_INPUT, 'Email format is invalid')
     }
