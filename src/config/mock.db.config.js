5caa3f9922aa2e4233b9a60be87020907fc885e6
piragna <gonzalovarelaalagna@gmail.com>
2025-09-10 11:58:47 -0300

    Removed one entry from mock db

--- src/config/mock.db.config.js
+++ src/config/mock.db.config.js
@@ -2,5 +2,4 @@
 
 export const mockUsers = [
   { id: 'u1', email: 'gonzalo@example.com', passwordHash: '985b883e66e9f181eede78d83eca52c67f6e571b82599d1c1da9681920e007df' },
-  { id: 'u2', email: 'ana@example.com', passwordHash: '1612919b91393fdab5c16a4a979b54f90b2870afa0772f26a1919d5e41fa7996' },
 ];
