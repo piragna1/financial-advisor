b1315c3c67458f3dfb8d9c3becaf6cd9826e7703
piragna <gonzalovarelaalagna@gmail.com>
2025-09-10 15:58:07 -0300

    🪵 - Leaving authController.mjs marked as pending - 👷🏻 Working on comparePasswords.js FILE

--- src/actors/retrievers/userRetriever.js
+++ src/actors/retrievers/userRetriever.js
@@ -1,6 +1,5 @@
 //userRetriever.js
 import {findUserByEmail} from '../../repositories/userRepo.js'
 export async function userRetrieve(email){
-    console.log('userRetriever.js -> userRetrieve()')
     return await findUserByEmail(email);
 }
