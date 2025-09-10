672f6b0b4b392101f1d05fe26daab5d509fba5dc
piragna <gonzalovarelaalagna@gmail.com>
2025-09-10 11:56:16 -0300

    ✏️ Making some changes in loan model...

--- src/config/loanConfig.js
+++ src/config/loanConfig.js
@@ -2,6 +2,11 @@ export const loan = {
     principal:100000, //amount borrowed,
     interestRate:5.0, //anual interest rate in %
     termYears:15, //duration of loan in years
-    paymentFrecuency: 1, //payments per year
-    startDate: '2025-08-01' //loan start date
+    paymentFrecuencyPerYear: 1, //payments per year
+    startDate: '2025-08-01', //loan start date
+    compoundingFrequencyPerYear:12,
+    gracePeriods :0,
+    balloonPayment:0,
+    loanType:'amortized',
+    currency:'usd'
 };
