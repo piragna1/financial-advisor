26d5108bad63ebfcd95f1e00abb85bd656d8b96b
piragna <gonzalovarelaalagna@gmail.com>
2025-09-12 14:03:35 -0300

    Update imports

--- src/middlewares/authMiddleware.js
+++ src/middlewares/authMiddleware.js
@@ -1,5 +1,5 @@
 import { extractToken } from "../actors/utils/auth/extractToken.js";
-import { tokenExists } from "../actors/utils/auth/tokenExists.js";
+import { tokenExists } from "../actors/auth/tokenExists.js";
 import { AppError } from "../errors/AppError.js";
 import { TokenErrors } from "../errors/tokenError.js";
 import { verifyToken } from "../utils/auth/verifyToken.js";
