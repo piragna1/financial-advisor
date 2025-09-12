3ebac676397cf9b005fe487d547af61fd968c783
piragna <gonzalovarelaalagna@gmail.com>
2025-09-12 13:48:48 -0300

    Formatted document

--- src/errors/tokenError.js
+++ src/errors/tokenError.js
@@ -1,37 +1,37 @@
 export const TokenErrors = {
-    MISSING_TOKEN:{
-        code:"MISSING_TOKEN",
-        message:"Missing token",
-        status:401
-    },
-    INVALID_HEADER:{
-        code:"INVALID_TOKEN",
-        message:'Invalid token',
-        status:401
-    },
-    INVALID_PAYLOAD:{
-        code:"INVALID_TOKEN",
-        message:'Invalid token',
-        status:401
-    },
-    INVALID_SIGNATURE:{
-        code:"INVALID_TOKEN_SIGNATURE",
-        message:'Invalid token',
-        status:401
-    },
-    EXPIRED_TOKEN:{
-        code:"TOKEN_EXPIRED",
-        message:'Invalid token',
-        status:401
-    },
-    INVALID_TOKEN:{
-        code:'INVALID_TOKEN',
-        message:'Invalid token',
-        status:401,
-    },
-    TOKEN_GEN_ERROR:{
-    code:'TOKEN_GEN_ERROR',
-    message:'Failed to generate the token',
-    status:500
-}
-}
+  MISSING_TOKEN: {
+    code: "MISSING_TOKEN",
+    message: "Missing token",
+    status: 401,
+  },
+  INVALID_HEADER: {
+    code: "INVALID_TOKEN",
+    message: "Invalid token",
+    status: 401,
+  },
+  INVALID_PAYLOAD: {
+    code: "INVALID_TOKEN",
+    message: "Invalid token",
+    status: 401,
+  },
+  INVALID_SIGNATURE: {
+    code: "INVALID_TOKEN_SIGNATURE",
+    message: "Invalid token",
+    status: 401,
+  },
+  EXPIRED_TOKEN: {
+    code: "TOKEN_EXPIRED",
+    message: "Invalid token",
+    status: 401,
+  },
+  INVALID_TOKEN: {
+    code: "INVALID_TOKEN",
+    message: "Invalid token",
+    status: 401,
+  },
+  TOKEN_GEN_ERROR: {
+    code: "TOKEN_GEN_ERROR",
+    message: "Failed to generate the token",
+    status: 500,
+  },
+};
