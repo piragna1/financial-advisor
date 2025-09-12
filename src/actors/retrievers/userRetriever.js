f50c5a99b0b3d8381e047631a1283ddba2008e73
piragna <gonzalovarelaalagna@gmail.com>
2025-09-12 11:32:36 -0300

    - Renamed: userRetrieve -> findUserByEmail - created findUserById

--- src/actors/retrievers/userRetriever.js
+++ src/actors/retrievers/userRetriever.js
@@ -1,5 +1,18 @@
 //userRetriever.js
-import {findUserByEmail} from '../../repositories/userRepo.js'
-export async function userRetrieve(email){ //checked
-    return await findUserByEmail(email);
+import { findUserByEmail, findUserById } from "../../repositories/userRepo.js";
+
+export async function findUserByEmail(email) {
+  //checked
+  return await findUserByEmail(email);
+}
+export async function findUserById(id) {
+  if (!id || typeof id != "string") {
+    throw new AppError(AuthErrors.MISSING_CREDENTIALS, "Id is missing");
+  }
+  if (typeof id != "string") {
+    throw new AppError(AuthErrors.INVALID_INPUT, "Id must be a string");
+  }
+  const userId = req.userId;
+  const user = await findUserById(userId); //semi-pure
+  return user;
 }
