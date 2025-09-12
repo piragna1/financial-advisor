3bfa51ecba4ced45089728c5f7027f92534e2354
piragna <gonzalovarelaalagna@gmail.com>
2025-09-12 13:49:24 -0300

    New error cathegory added: TOKEN

--- src/errors/authErrors.js
+++ src/errors/authErrors.js
@@ -1,5 +1,5 @@
 export const AuthErrors = {
-  LOGIN:{
+  LOGIN: {
     INVALID_CREDENTIALS: {
       code: "INVALID_CREDENTIALS",
       message: "Invalid email or password",
@@ -11,7 +11,7 @@ export const AuthErrors = {
       status: 404,
     },
   },
-  REGISTER:{
+  REGISTER: {
     INVALID_INPUT: {
       code: "INVALID_INPUT",
       message: "Invalid field/s format",
@@ -27,11 +27,48 @@ export const AuthErrors = {
       message: "User is currently not available",
       status: 409,
     },
-    CREATION_FAILED:{
-        code:"CREATION_FAILED",
-        message:"An error has ocurred during registration",
-        status:500
-    }
+    CREATION_FAILED: {
+      code: "CREATION_FAILED",
+      message: "An error has ocurred during registration",
+      status: 500,
+    },
+  },
+  TOKEN: {
+    MISSING_TOKEN: {
+      code: "MISSING_TOKEN",
+      message: "Missing token",
+      status: 401,
+    },
+    INVALID_HEADER: {
+      code: "INVALID_TOKEN",
+      message: "Invalid token",
+      status: 401,
+    },
+    INVALID_PAYLOAD: {
+      code: "INVALID_TOKEN",
+      message: "Invalid token",
+      status: 401,
+    },
+    INVALID_SIGNATURE: {
+      code: "INVALID_TOKEN_SIGNATURE",
+      message: "Invalid token",
+      status: 401,
+    },
+    EXPIRED_TOKEN: {
+      code: "TOKEN_EXPIRED",
+      message: "Invalid token",
+      status: 401,
+    },
+    INVALID_TOKEN: {
+      code: "INVALID_TOKEN",
+      message: "Invalid token",
+      status: 401,
+    },
+    TOKEN_GEN_ERROR: {
+      code: "TOKEN_GEN_ERROR",
+      message: "Failed to generate the token",
+      status: 500,
+    },
   },
 };
 
