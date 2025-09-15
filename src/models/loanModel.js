0dd9988595d8b16c5a7cd2a6e38e1cf66b03f4f6
piragna <gonzalovarelaalagna@gmail.com>
2025-09-15 14:53:22 -0300

    Incrementing loan capabilities' scope

---
+++ src/models/loanModel.js
@@ -0,0 +1,13 @@
+export const loan = {
+    financialProfileId:String,
+    startDate: Date,
+    termYears:Number,
+    principal:Number,
+    interestRate:Number,
+    paymentFrecuencyPerYear: Number,
+    compoundingFrequencyPerYear:Number,
+    gracePeriods :Number,
+    balloonPayment:Number,
+    loanType:String,
+    currency:String
+};
