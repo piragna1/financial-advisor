0b782d11109053335221c24977e3424ddea608a8
piragna <gonzalovarelaalagna@gmail.com>
2025-09-16 10:34:02 -0300

    Removing decimals from numbers calculations' results

--- src/services/loan/loanCalculator.js
+++ src/services/loan/loanCalculator.js
@@ -29,7 +29,7 @@ function calculateMaxLoanCapacity(loan){
     }
     const maxLoanCapacity = repaymentCapacity/(1+interestRate);
     console.log(`User max loan capacity over ${loanTermYears} years: $${maxLoanCapacity.toLocaleString()} with an interest rate of % ${interestRate*100}.`);
-    return maxLoanCapacity;
+    return maxLoanCapacity.toFixed(0);
 };
 
 function calculateCompoundInterest(loan){
