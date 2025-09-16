2c5e8eb3d7b31bd8004085020e95e661c21b6d2d
piragna <gonzalovarelaalagna@gmail.com>
2025-09-16 08:16:56 -0300

    Added a comment explaining when lastName field is provided.

--- src/models/userModel.js
+++ src/models/userModel.js
@@ -1,7 +1,7 @@
 const User = {
   id: String,               // Unique identifier
   name: String,             // Provided at sign-up
-  lastName:String,
+  lastName:String,          // Provided at sign-up
   email: String,            // Used for login and uniqueness
   passwordHash: String,     // Hashed password for security
   createdAt: Date,          // Timestamp of account creation
