d1432a6f20fcbaf80f040ac74edfb8a7ca572da5
piragna <gonzalovarelaalagna@gmail.com>
2025-09-18 09:37:11 -0300

    Removing `preferences` field for now

--- src/models/profileModel.js
+++ src/models/profileModel.js
@@ -2,13 +2,13 @@ export const profile = {
   userId: String,              // FK to User model (uuid)
   firstName: String,
   lastName: String,
-  birthDate: String,
+  birthDate: String || undefined,
   location: String || undefined,
   language: String || 'en',
   avatarUrl: String || undefined,
   bio: String,
-  preferences: {
-    theme: String || 'light',
-    notifications: Boolean || true
-  }
+  // preferences: {
+  //   theme: String || 'light',
+  //   notifications: Boolean || true
+  // }
 }
