d862f9054cc903b963685767ed22a4f83e2d265a
piragna <gonzalovarelaalagna@gmail.com>
2025-09-19 10:38:15 -0300

    Dfined payment entity builder modeling

--- src/entities/paymentEntity.js
+++ src/entities/paymentEntity.js
@@ -1,26 +1,22 @@
 import { v4 } from "uuid";
 
-export function buildPaymentEntity(data){
-    if (!data.scheduleId)
-        throw new Errror('Schedule input is missing')
-    if (typeof data.amount !== 'number'||
-        data.amount <= 0
-    )
-        throw new Error("Invalid amount received")
-    if (!data.method) 
-        throw new Error('Payment method is missing')
-    
-    
-    return {
-        id:v4(),
-        scheduleId:data.scheduleId,
-        dueDate:Date.now(),
-        amount:data.amount,
-        currency:data.currency || 'USD',
-        status: (data.amount === 0) ? 'pending' : 'paid',
-        paidAt: (data.amount === 0) ? Date.now() : undefined,
-        method: data.method,
-        reference: data.reference,
-        notes:data.notes
-    }
+export function buildPaymentEntity(data) {
+  if (!data.scheduleId) throw new Error("Schedule input is missing");
+  if (typeof data.amount !== "number" || data.amount < 0)
+    throw new Error("Payment amount is invalid");
+  if (!data.method) throw new Error("Payment method is missing");
+  if (!data.dueDate) throw new Error("Due date is missing");
+
+  return {
+    id: v4(),
+    scheduleId: data.scheduleId,
+    dueDate: data.dueDate,
+    amount: data.amount,
+    currency: data.currency || "USD",
+    status: data.status || "pending",
+    paidAt: data.status === "paid" ? Date.now() : undefined,
+    method: data.method,
+    reference: data.reference,
+    notes: data.notes,
+  };
 }
