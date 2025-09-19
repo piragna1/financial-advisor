e8b65897679dcbf5471e9f3b9d26155bad8c6ac5
piragna <gonzalovarelaalagna@gmail.com>
2025-09-19 08:54:48 -0300

    Profile builder created.

--- src/entities/profileEntity.js
+++ src/entities/profileEntity.js
@@ -1,14 +1,25 @@
 export function buildProfileEntity(data){
     if (!data.userId) throw new Error('User id missing.');
-    const {userId,firstName,lastName,birthDate,location,language,avatarUrl,bio} = data;
-    const profile = {};
-    //All fields are optional and have or not a default value.
-    if (firstName) profile.firstName = firstName;
-    if (lastName) profile.lastName = lastName;
-    if (birthDate) profile.birthDate = birthDate;
-    if (location) profile.location = location;
-    if (language) profile.language = language;
-    if (avatarUrl) profile.avatarUrl = avatarUrl;
-    if (bio) profile.bio = bio;
+    const {
+        userId,
+        firstName = 'Dear',
+        lastName = 'User',
+        birthDate,
+        location,
+        language='en',
+        avatarUrl,
+        bio
+    } = data;
+
+    const profile = {
+        userId,
+        ...(firstName && {firstName}),
+        ...(lastName && {lastName}),
+        ...(birthDate && {birthDate}),
+        ...(location && {location}),
+        ...(language && {language}),
+        ...(avatarUrl && {avatarUrl}),
+        ...(bio && {bio})
+    };
     return profile;
 }
