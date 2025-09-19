19051ed5a24199e5864077ff2ae6428bcd49e84b
piragna <gonzalovarelaalagna@gmail.com>
2025-09-19 09:14:43 -0300

    FinancialProfile entity builder created.

--- src/entities/financialProfileEntity.js
+++ src/entities/financialProfileEntity.js
@@ -1,1 +1,14 @@
-export function buildFinancialProfileEntity(data){}
+import {v4} from 'uuid'
+
+export function buildFinancialProfileEntity(data){
+    if (!data.userId || !data.id) 
+        throw new Error('Fin. profile or/and user id is/are missing');
+    if(typeof salary !=='number' || salary <=0)
+        throw new Error("Invalid salary data received");
+    
+    return {
+        id:v4(),
+        userId:data.userId,
+        salary:data.salary
+    }
+}
