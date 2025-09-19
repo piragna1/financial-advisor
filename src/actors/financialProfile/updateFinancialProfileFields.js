28a48e4bf604f8614609fb608262127f2229d4be
piragna <gonzalovarelaalagna@gmail.com>
2025-09-19 14:48:36 -0300

    Created actor for modifying financial profile values

---
+++ src/actors/financialProfile/updateFinancialProfileFields.js
@@ -0,0 +1,17 @@
+export function updateFinancialProfileFields(profile,updates){
+    const allowed = ['salary'];
+    const sanitized = {};
+    for (key of allowed){
+        if (key in updates){
+            sanitized[key] = updates[key];
+        }
+    }
+    if (Object.keys(sanitized) === 0 )
+        throw new Error('No valid financial fields to update')
+
+    return {
+        ...profile,
+        ...sanitized,
+        updatedAt:Date.now()
+    };
+}
