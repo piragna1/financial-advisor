cf4e94eb2137046e520e047112bf5fcb93cd1d95
piragna <gonzalovarelaalagna@gmail.com>
2025-09-19 10:02:05 -0300

    Creating model entity builder

--- src/entities/loanEntity.js
+++ src/entities/loanEntity.js
@@ -1,1 +1,30 @@
-export function buildLoanEntity(data){}
+import { v4 } from "uuid";
+
+export function buildLoanEntity(data){
+    if (!data.financialProfileId)
+        throw new Error('Financial profile id missing');
+    if(
+        typeof data.principal !== 'number' || 
+        data.principal <=0
+    ) 
+        throw new Error('Invalid principal amount')
+    if (
+        typeof data.interestRate !== 'number' || 
+        data.interestRate <=0
+    )
+        throw new Error("Invalid interest rate input")
+    return {
+        id:v4(),
+        financialProfileId:data.financialProfileId,
+        startDate: (data.startDate || Date.now()),
+        termYears: (data.termYears || 1),
+        principal:data.principal,
+        interestRate:data.interestRate,
+        paymentFrequencyPerYear: (data.paymentFrequencyPerYear || 12),
+        compoundingFrequencyPerYear: (data.compoundingFrequencyPerYear || 12),
+        gracePeriodMonths: data.gracePeriodMonths ?? 0,
+        balloonPayment: data.balloonPayment ?? 0,
+        loanType: data.loanType || 'personal',
+        currency: data.currency || 'USD'
+    }
+}
