b8b61072c3383e9e1db0cdff4b7d5cd7af640695
piragna <gonzalovarelaalagna@gmail.com>
2025-09-22 09:22:00 -0300

    Loan validator

--- src/actors/validators/loan/validateLoanInput.js
+++ src/actors/validators/loan/validateLoanInput.js
@@ -1,4 +1,5 @@
 export function validateLoanInput(loan){
+    if (!loan || typeof loan !== 'object') throw new Error("Invalid loan structure received");
     if (!loan.id) throw new Error("Invalid loan id");
     if (!loan.financialProfileId) throw new Error("Invalid financial profile id");
     if (!loan.startDate) loan.startDate = Date.now();
@@ -9,7 +10,7 @@ export function validateLoanInput(loan){
     if (typeof loan.compoundingFrequencyPerYear !== 'number') // check
     if (typeof loan.gracePeriodMonths !== 'number' || loan.gracePeriodMonths < 0) loan.gracePeriodMonths = 0;
     if (typeof loan.balloonPayment !== 'number' || loan.balloonPayment < 0) loan.balloonPayment = 0;
-    if (!loan.loanType || typeof loan.loanType !== 'string') throw new Error("Invalid loan type received")
-    if (!loan.currency || typeof loan.currency !== 'string') throw new Error('Invalid currency received')
+    if (!loan.loanType || typeof loan.loanType !== 'string') throw new Error("Invalid loan type received");
+    if (!loan.currency || typeof loan.currency !== 'string') throw new Error('Invalid currency received');
     //currency
 }
