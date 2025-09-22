c13dfc33ffe1a7b426c4e1051051563835081209
piragna <gonzalovarelaalagna@gmail.com>
2025-09-22 09:57:50 -0300

    Update: instead of modifying on the fly the object in this pure actor, it would be preferred to just throw an error instead

--- src/actors/validators/loan/validateLoanInput.js
+++ src/actors/validators/loan/validateLoanInput.js
@@ -2,14 +2,14 @@ export function validateLoanInput(loan){
     if (!loan || typeof loan !== 'object') throw new Error("Invalid loan structure received");
     if (!loan.id) throw new Error("Invalid loan id");
     if (!loan.financialProfileId) throw new Error("Invalid financial profile id");
-    if (!loan.startDate) loan.startDate = Date.now();
-    if (typeof loan.termYears !== 'number' || loan.termYears <= 0) loan.termYears = 1;
-    if (typeof loan.principal !== 'number' || loan.principal <=0 ) loan.principal = 25000;
-    if (typeof loan.interestRate !== 'number' || loan.interestRate<=0) loan.interestRate = 0.05;
-    if (typeof loan.paymentFrequencyPerYear !== 'number' || loan.paymentFrequencyPerYear <= 0) paymentFrequencyPerYear = 12;
-    if (typeof loan.compoundingFrequencyPerYear !== 'number') // check
-    if (typeof loan.gracePeriodMonths !== 'number' || loan.gracePeriodMonths < 0) loan.gracePeriodMonths = 0;
-    if (typeof loan.balloonPayment !== 'number' || loan.balloonPayment < 0) loan.balloonPayment = 0;
+    if (!loan.startDate || typeof loan.startDate !== 'number') throw new Error("Invalid start date value received");
+    if (typeof loan.termYears !== 'number' || loan.termYears <= 0) throw new Error("Invalid term years value received");
+    if (typeof loan.principal !== 'number' || loan.principal <=0 ) throw new Error("Invalid principal received");
+    if (typeof loan.interestRate !== 'number' || loan.interestRate<=0) throw new Error("Invalid loan's interest rate value received");
+    if (typeof loan.paymentFrequencyPerYear !== 'number' || loan.paymentFrequencyPerYear <= 0) throw new Error("Invalid payment frequency per year received");
+    if (typeof loan.compoundingFrequencyPerYear !== 'number' || loan.compoundingFrequencyPerYear <=0) throw new Error("Invalid compounding frequency per year received");
+    if (typeof loan.gracePeriodMonths !== 'number' || loan.gracePeriodMonths < 0)throw new Error("Invalid grace periods' value received");
+    if (typeof loan.balloonPayment !== 'number' || loan.balloonPayment < 0) throw new Error("Invalid balloon payment's value received");
     if (!loan.loanType || typeof loan.loanType !== 'string') throw new Error("Invalid loan type received");
     if (!loan.currency || typeof loan.currency !== 'string') throw new Error('Invalid currency received');
 }
