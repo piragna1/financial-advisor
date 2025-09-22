25e5bddcea76c3e7fc3022a727b1aeb9bb84fbb4
piragna <gonzalovarelaalagna@gmail.com>
2025-09-22 15:38:05 -0300

    Updating routes. Adding new routes.

--- src/routes/loan/loanRoutes.js
+++ src/routes/loan/loanRoutes.js
@@ -1,9 +1,13 @@
 import express from 'express';
-import { createLoanController, simulateLoanController } from '../../controllers/loanController.mjs';
+import { createLoanController, deleteLoanController, getLoanByIdController, getLoansController, simulateLoanController, updateLoanByIdController } from '../../controllers/loanController.mjs';
 
 const router = express.Router();
 
 router.post('/simulate-loan', simulateLoanController);
 router.post('/create-loan', createLoanController);
-
+router.get('/:id', getLoanByIdController);
+router.put('/:id', updateLoanByIdController);
+router.post('/', deleteLoanController);
+router.delete('/:id', deleteLoanController);
+router.get('/',getLoansController);
 export default router;
