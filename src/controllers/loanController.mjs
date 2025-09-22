df51c05455a2319faeef5fbceb7fcac382c97387
piragna <gonzalovarelaalagna@gmail.com>
2025-09-22 15:39:58 -0300

    Updating imports and implementing the controllers for the CRUD for loans

--- src/controllers/loanController.mjs
+++ src/controllers/loanController.mjs
@@ -1,5 +1,8 @@
-import { createLoan } from "../actors/loan/createLoan";
-import { simulateLoan } from "../actors/loan/simulateLoan";
+import { createLoan } from "../actors/loan/createLoan.js";
+import { simulateLoan } from "../actors/loan/simulateLoan.js";
+import { AppError } from "../errors/AppError";
+import { LoanErrors } from "../errors/loanErrors";
+import {deleteLoan, getLoanById, getLoans, updateLoan} from '../repositories/loanRepository.js'
 export async function simulateLoanController(req,res,next){
     try {
         const simulation = simulateLoan(req.body, req.financialProfile);
@@ -15,5 +18,51 @@ export async function createLoanController(req,res,next) {
         res.status(201).json(loan);
     } catch (error) {
         next(error);
+    }
+}
+
+export async function getLoanByIdController(req,res,next) {
+    try {
+        const {id} = req.params;
+        const loan = await getLoanById();
+
+        if (!loan){
+            throw new AppError(LoanErrors.FIND.NOT_FOUND);
+        }
+        
+        res.status(200).json(loan);
+    } catch (error) {
+        next(error)
+    }
+}
+
+export async function updateLoanByIdController(req,res,next) {
+    try {
+        const {id} =req.params;
+        const updates = req.body;
+        const updatedLoan = await updateLoan(id,updates);
+        res.status(200).json(updatedLoan);
+    } catch (error) {
+        next(error)
+    }
+}
+
+export async function deleteLoanController(req,res,next) {
+    try {
+        const {id} = req.params;
+        const deletedLoan = await deleteLoan(id);
+        res.status(200).json(deletedLoan);
+    } catch (error) {
+        next(error)
+    }
+    
+}
+
+export async function getLoansController(res,req,next) {
+    try {
+        const loans = getLoans();
+        res.status(200).json(loans);
+    } catch (error) {
+        next(error)
     }
 }
