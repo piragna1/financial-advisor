4ea05a68acc246832a18fc940e8057e3bc1790a5
piragna <gonzalovarelaalagna@gmail.com>
2025-09-22 09:57:14 -0300

    Created new file

---
+++ src/errors/loanErrors.js
@@ -0,0 +1,6 @@
+export const LoanErrors = {
+  INVALID_STRUCTURE: { code: 'INVALID_STRUCTURE', status: 400 },
+  INVALID_PRINCIPAL: { code: 'INVALID_PRINCIPAL', status: 400 },
+  INVALID_TERM: { code: 'INVALID_TERM', status: 400 },
+  INVALID_RATE: { code: 'INVALID_RATE', status: 400 }
+};
