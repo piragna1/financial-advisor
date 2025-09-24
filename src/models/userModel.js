c91fe5822f4067f4714bf9ca0fac72e4b8de39fe
piragna <gonzalovarelaalagna@gmail.com>
2025-09-24 16:23:41 -0300

    Delete comments

--- src/models/userModel.js
+++ src/models/userModel.js
@@ -1,7 +1,7 @@
 const User = {
-  id: String,               // Unique identifier
-  email: String,            // Used for login and uniqueness
-  passwordHash: String,     // Hashed password for security
-  createdAt: Date,          // Timestamp of account creation
-  updatedAp:Date
+  id: String,           
+  email: String,          
+  passwordHash: String,    
+  createdAt: Date,         
+  updatedAt:Date
 };
