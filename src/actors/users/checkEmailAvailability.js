05bf32de6adea8459df0bc507ef150a197a17e6c
piragna <gonzalovarelaalagna@gmail.com>
2025-09-25 09:21:06 -0300

    Normalizing emails in queries for registering

--- src/actors/users/checkEmailAvailability.js
+++ src/actors/users/checkEmailAvailability.js
@@ -3,14 +3,16 @@ import { AppError } from "../../errors/AppError.js";
 import { AuthErrors } from "../../errors/authErrors.js";
 
 export async function checkEmailAvailability(email) {
-  if (!email || typeof email !== "string")
+  if (!email || typeof email !== "string" || email.trim() === '')
     throw new AppError(AuthErrors.REGISTER.INVALID_INPUT);
+  
+  email = email.trim();
 
-  const query = `
-    SELECT 1 FROM users
-    WHERE email = $1
-    LIMIT 1;
-  `;
+ const query = `
+  SELECT 1 FROM users
+  WHERE LOWER(email) = LOWER($1)
+  LIMIT 1;
+`;
 
   const result = await pool.query(query, [email]);
   if (result.rowCount > 0)
