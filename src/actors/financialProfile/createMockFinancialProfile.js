44c59a3c074dde283cc090ee5be0a372eb768e42
piragna <gonzalovarelaalagna@gmail.com>
2025-09-25 15:21:28 -0300

    Implementation of creating a mock financial profile and inserting it into db

--- src/actors/financialProfile/createMockFinancialProfile.js
+++ src/actors/financialProfile/createMockFinancialProfile.js
@@ -1,17 +1,19 @@
 import { v4 as uuidv4 } from "uuid";
 import { pool } from "../../db/pool.js";
 
-export async function createMockFinancialProfile() {
-  const id = uuidv4();
+export async function createMockFinancialProfile(overrides = {}) {
+  const id = overrides.id || uuidv4();
+  const userId = overrides.userId || "user-mock";
+  const salary = overrides.salary || 50000;
   const now = new Date();
 
   const result = await pool.query(
     `
-            INSERT INTO financial_profiles(id,user_id,salary,created_at,updated_at)
-            VALUES ($1,$2,$3,$4,$5)
-            RETURNING *;
+      INSERT INTO financial_profiles(id, user_id, salary, created_at, updated_at)
+      VALUES ($1, $2, $3, $4, $5)
+      RETURNING *;
     `,
-    [id, "user-mock", 50000, now, now]
+    [id, userId, salary, now, now]
   );
 
   return result.rows[0];
