f0fb6484283e0fe1acfc63da921eb15600fa63c5
piragna <gonzalovarelaalagna@gmail.com>
2025-09-25 12:15:29 -0300

    Modified for generating unique emails using user id

--- src/actors/users/createMockUser.js
+++ src/actors/users/createMockUser.js
@@ -1,8 +1,11 @@
 import { pool } from "../../db/pool";
 
+
 export async function createMockUser(userId) {
+  const email = `${userId}@example.com`; // ← email único por usuario
   await pool.query(`
     INSERT INTO users (id, email, password_hash, created_at)
     VALUES ($1, $2, $3, NOW())
-  `, [userId, "mock@example.com", "hashed-password"]);
+  `, [userId, email, "hashed-password"]);
 }
+
