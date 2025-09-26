b2b6e5e7d90054785eaddea11b61ad6de5701a5c
piragna <gonzalovarelaalagna@gmail.com>
2025-09-26 12:40:48 -0300

    New helper fn for validating uuids

--- src/tests/helpers/testHelpers.js
+++ src/tests/helpers/testHelpers.js
@@ -41,3 +41,7 @@ export function utcDate(ymd) {
   const [year, month, day] = ymd.split("-").map(Number);
   return new Date(Date.UTC(year, month - 1, day));
 }
+
+export function isValidUUID(str) {
+  return /^[0-9a-f]{8}-[0-9a-f]{4}-[1-5][0-9a-f]{3}-[89ab][0-9a-f]{3}-[0-9a-f]{12}$/i.test(str);
+}
