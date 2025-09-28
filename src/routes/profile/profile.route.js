f89343e0b232f0059b2d612010a39ae8e4ae2e98
piragna <gonzalovarelaalagna@gmail.com>
2025-09-28 12:58:10 -0300

    Debug log

--- src/routes/profile/profile.route.js
+++ src/routes/profile/profile.route.js
@@ -1,8 +1,19 @@
 import express from 'express';
 import { authMiddleware } from '../../middlewares/authMiddleware.js';
-import { getProfile, updateProfileController } from '../../controllers/profileController.mjs';
+import { getProfile, updateProfileController, createProfileController } from '../../controllers/profileController.mjs';
 
 const router = express.Router();
+
+
+router.post("/", (req, res, next) => {
+  console.log("Profile route hit");
+  next();
+}, createProfileController);
+
+
+
+
+router.post("/", createProfileController);
 
 router.get(`/`, authMiddleware, getProfile);
 
