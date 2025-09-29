adaa1358b8b90c6e5163e80c1ad4ad14a2a7c872
piragna <gonzalovarelaalagna@gmail.com>
2025-09-29 10:28:07 -0300

    Added route for updating an user

--- src/routes/user/userRoutes.js
+++ src/routes/user/userRoutes.js
@@ -1,11 +1,16 @@
 import express from "express";
-import { deleteUserController, listUsersController } from "../../controllers/userController.mjs";
+import { deleteUserController, listUsersController, updateUserController } from "../../controllers/userController.mjs";
+import { authMiddleware } from "../../middlewares/authMiddleware.js";
 
 
 const router = express.Router();
 
-router.delete("/:email", deleteUserController);
 
 router.get("/", listUsersController);
+
+router.put('/', authMiddleware,updateUserController);
+
+router.delete("/:email", deleteUserController);
+
 
 export default router;
