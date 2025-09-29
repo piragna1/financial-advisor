a2cf05f82574f7e51aab5faaba5757247fe556db
piragna <gonzalovarelaalagna@gmail.com>
2025-09-29 19:31:07 -0300

    Update imports

--- src/actors/users/createMockUser.js
+++ src/actors/users/createMockUser.js
@@ -1,15 +1,15 @@
-import { pool } from "../../db/pool.js";
+import { pool } from "../../db/pool.mjs";
 
 
 export async function createMockUser(userId) {
   const email = `${userId}@example.com`; // ← email único por usuario
 
-  console.log('problemas qui con el password_hash?')
-
-  await pool.query(`
+  const result = await pool.query(`
     INSERT INTO users (id, email, password_hash, created_at, updated_at)
     VALUES ($1, $2, $3, NOW(), NOW())
   `, [userId, email, "hashedPassword"]);
+
+
     return { id: userId, email };
 }
 
