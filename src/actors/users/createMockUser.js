b3ca87d6c8116e09b8c6e507c158313876d18321
piragna <gonzalovarelaalagna@gmail.com>
2025-09-29 18:08:36 -0300

    Trying to debug

--- src/actors/users/createMockUser.js
+++ src/actors/users/createMockUser.js
@@ -3,10 +3,13 @@ import { pool } from "../../db/pool.js";
 
 export async function createMockUser(userId) {
   const email = `${userId}@example.com`; // ← email único por usuario
+
+  console.log('problemas qui con el password_hash?')
+
   await pool.query(`
-    INSERT INTO users (id, email, password_hash, created_at)
-    VALUES ($1, $2, $3, NOW())
-  `, [userId, email, "hashed-password"]);
+    INSERT INTO users (id, email, password_hash, created_at, updated_at)
+    VALUES ($1, $2, $3, NOW(), NOW())
+  `, [userId, email, "hashedPassword"]);
     return { id: userId, email };
 }
 
