627229f518b856fefc74fc4305ff474b17a09e8a
piragna <gonzalovarelaalagna@gmail.com>
2025-09-30 13:18:31 -0300

    update imports

--- src/tests/unit/repositories/user/deleteUser.test.js
+++ src/tests/unit/repositories/user/deleteUser.test.js
@@ -1,6 +1,6 @@
 import { v4 as uuidv4 } from "uuid";
-import { pool } from "../../../../db/pool.js";
-import { saveUser, deleteUser } from "../../../repositories/userRepository.js";
+import { pool } from "../../../../db/pool.mjs";
+import { saveUser, deleteUser } from "../../../../repositories/userRepository.js";
 
 describe("deleteUser(id)", () => {
   const baseUser = {
