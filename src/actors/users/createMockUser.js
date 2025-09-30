26162554dfb87340b7bf7c74086d7baa1411c765
piragna <gonzalovarelaalagna@gmail.com>
2025-09-30 13:07:10 -0300

    Adding a default value

--- src/actors/users/createMockUser.js
+++ src/actors/users/createMockUser.js
@@ -1,7 +1,7 @@
 import { pool } from "../../db/pool.mjs";
 
 
-export async function createMockUser(userId, email = `${userId}@example.com`) {
+export async function createMockUser(userId, email = `${userId}@example.com`, passwordHash = 'hashedPassword') {
 
 if (!email || typeof email !== "string") {
   throw new Error("createMockUser requires a valid email");
@@ -11,7 +11,7 @@ if (!email || typeof email !== "string") {
   await pool.query(`
     INSERT INTO users (id, email, password_hash, created_at, updated_at)
     VALUES ($1, $2, $3, NOW(), NOW())
-  `, [userId, email, "hashedPassword"]);
+  `, [userId, email, passwordHash]);
 
   
 const check = await pool.query("SELECT * FROM users WHERE id = $1", [userId]);
