3628de1c8a4003f9ffc605ac220e1b454448f13e
piragna <gonzalovarelaalagna@gmail.com>
2025-09-30 13:18:39 -0300

    update imports

--- src/actors/users/checkEmailAvailability.js
+++ src/actors/users/checkEmailAvailability.js
@@ -1,4 +1,4 @@
-import { pool } from "../../db/pool.js";
+import { pool } from "../../db/pool.mjs";
 import { AppError } from "../../errors/AppError.js";
 import { AuthErrors } from "../../errors/authErrors.js";
 
