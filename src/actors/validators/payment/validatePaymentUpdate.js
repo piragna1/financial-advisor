7f6e1ec5b83ea98dc23f26278af44529c60cc2ac
piragna <gonzalovarelaalagna@gmail.com>
2025-10-01 19:44:21 -0300

    rmv code

--- src/actors/validators/payment/validatePaymentUpdate.js
+++ src/actors/validators/payment/validatePaymentUpdate.js
@@ -3,12 +3,11 @@ import { PaymentErrors } from "../../../errors/paymentErrors.js";
 import { isValidUUID } from "../../../tests/helpers/testHelpers.js";
 import { validatePaymentInput } from "./validatePaymentInput.js";
 export function validatePaymentUpdate(payment) {
+
+
   try {
-    validatePaymentInput(payment);
+    validatePaymentInput(payment, true);
   } catch (err) {
-    if (err.code === PaymentErrors.CREATE.INVALID_DATA.code) {
-      throw new AppError(PaymentErrors.UPDATE.INVALID_DATA);
-    }
     throw err;
   }
 }
