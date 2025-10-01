95c226fe625f060a31f7a4e56c0f2c1a1158ad40
piragna <gonzalovarelaalagna@gmail.com>
2025-10-01 15:52:58 -0300

    Trying to pass test

--- src/tests/unit/repositories/profile/updateProfile.test.js
+++ src/tests/unit/repositories/profile/updateProfile.test.js
@@ -20,6 +20,9 @@ describe("updateProfile(profile)", () => {
   });
 
   it("should update all editable fields", async () => {
+    console.log('should update all editable fields')
+    const baseUser = await createMockUser(uuidv4());
+    const userId = baseUser.id;
     const profile = {
       id: uuidv4(),
       userId,
@@ -27,41 +30,41 @@ describe("updateProfile(profile)", () => {
       lastName: "Name",
       bio: "Original bio",
     };
-    
-    const userId = uuidv4();
-    await createMockUser(userId);
     await createProfile(profile);
-
     const updated = {
-  id: profile.id,
-  firstName: "Updated",
-  lastName: "Surname",
-  birthDate: "1995-05-05", // ← string plano
-  location: "Buenos Aires",
-  language: "es",
-  avatarUrl: "https://updated.com/avatar.png",
-  bio: "Updated bio"
-};
+      id: profile.id,
+      userId,
+      firstName: "Updated",
+      lastName: "Surname",
+      birthDate: "1995-05-05", // ← string plano
+      location: "Buenos Aires",
+      language: "es",
+      avatarUrl: "https://updated.com/avatar.png",
+      bio: "Updated bio",
+    };
 
-const result = await updateProfile(updated);
+    const result = await updateProfile(updated);
+    console.log('RESULT ->', result)
+    console.log('RESULT.avatar_url ->', result['avatar_url'])
+    console.log('RESULT.avatarUrl ->', result['avatarUrl'])
 
-expect(result).toMatchObject({
-  id: profile.id,
-  first_name: "Updated",
-  last_name: "Surname",
-  location: "Buenos Aires",
-  language: "es",
-  avatar_url: "https://updated.com/avatar.png",
-  bio: "Updated bio"
-});
+    expect(result['id']).toBe(profile.id)
+    expect(result.user_id).toBe(profile.userId)
+    expect(result.first_name).toBe(updated.firstName)
+    expect(result.last_name).toBe(updated.lastName)
+    expect(result.location).toBe(updated.location)
+    expect(result.language).toBe(updated.language)
+    expect(result.avatar_url).toBe(updated.avatarUrl)
+    expect(result.bio).toBe(updated.bio)
 
-expect(new Date(result.birth_date).toISOString().slice(0, 10)).toBe("1995-05-05");
-
-
+    expect(new Date(result.birth_date).toISOString().slice(0, 10)).toBe(
+      "1995-05-05"
+    );
   });
 
   it("should trim string fields before updating", async () => {
     const userId = uuidv4();
+    await createMockUser(userId);
     const profile = {
       id: uuidv4(),
       userId,
@@ -69,12 +72,11 @@ expect(new Date(result.birth_date).toISOString().slice(0, 10)).toBe("1995-05-05"
       lastName: "Test ",
       bio: "  T rim test ",
     };
-
-    await createMockUser(userId);
     await createProfile(profile);
 
     const updated = {
       id: profile.id,
+      userId,
       firstName: "  Trimmed  ",
       lastName: "  Surname  ",
       bio: "  Updated bio  ",
@@ -88,13 +90,11 @@ expect(new Date(result.birth_date).toISOString().slice(0, 10)).toBe("1995-05-05"
 
   it("should default missing optional fields to null or 'en'", async () => {
 
-    console.log('should default missing optional fields to null or "en"')
-
     const baseUser = await createMockUser(uuidv4());
 
     const profile = {
       id: uuidv4(),
-      userId:baseUser.id,
+      userId: baseUser.id,
       firstName: "Minimal",
       lastName: "User",
       bio: "Minimal bio",
@@ -102,10 +102,9 @@ expect(new Date(result.birth_date).toISOString().slice(0, 10)).toBe("1995-05-05"
 
     const baseProfile = await createProfile(profile);
 
-    
     const updated = {
       id: baseProfile.id,
-      userId:profile.userId,
+      userId: profile.userId,
       firstName: "Minimal",
       lastName: "User",
     };
@@ -148,7 +147,7 @@ expect(new Date(result.birth_date).toISOString().slice(0, 10)).toBe("1995-05-05"
     await expect(updateProfile(profile)).rejects.toMatchObject({
       code: "PROFILE_UPDATE_NOT_FOUND",
       message: "Profile not found",
-      status: 404
+      status: 404,
     });
   });
 });
