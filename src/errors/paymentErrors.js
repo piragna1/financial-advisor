8d69e59693366bf832d8da85af667dd99bd0e90c
piragna <gonzalovarelaalagna@gmail.com>
2025-10-07 09:28:00 -0300

    Document formatted

--- src/errors/paymentErrors.js
+++ src/errors/paymentErrors.js
@@ -3,53 +3,53 @@ export const PaymentErrors = {
     INVALID_ID: {
       code: "PAYMENT_CREATE_INVALID_ID",
       message: "Invalid or missing payment ID",
-      status: 400
+      status: 400,
     },
     INVALID_DATA: {
       code: "PAYMENT_CREATE_INVALID_DATA",
       message: "Missing or invalid payment data",
-      status: 400
-    }
+      status: 400,
+    },
   },
   READ: {
     INVALID_ID: {
       code: "PAYMENT_READ_INVALID_ID",
       message: "Invalid or missing payment ID",
-      status: 400
+      status: 400,
     },
     NOT_FOUND: {
       code: "PAYMENT_READ_NOT_FOUND",
       message: "Payment not found",
-      status: 404
-    }
+      status: 404,
+    },
   },
   UPDATE: {
     INVALID_ID: {
       code: "PAYMENT_UPDATE_INVALID_ID",
       message: "Invalid or missing payment ID",
-      status: 400
+      status: 400,
     },
     NOT_FOUND: {
       code: "PAYMENT_UPDATE_NOT_FOUND",
       message: "Payment not found",
-      status: 404
-    },INVALID_DATA: {
-  code: "UPDATE_INVALID_DATA",
-  message: "Missing or invalid payment data for update",
-  status: 400
-}
-
+      status: 404,
+    },
+    INVALID_DATA: {
+      code: "UPDATE_INVALID_DATA",
+      message: "Missing or invalid payment data for update",
+      status: 400,
+    },
   },
   DELETE: {
     INVALID_ID: {
       code: "PAYMENT_DELETE_INVALID_ID",
       message: "Invalid or missing payment ID",
-      status: 400
+      status: 400,
     },
     NOT_FOUND: {
       code: "PAYMENT_DELETE_NOT_FOUND",
       message: "Payment not found",
-      status: 404
-    }
-  }
+      status: 404,
+    },
+  },
 };
