90ef9788cd0dfee862f1abf291dd861c011b5aa0
piragna <gonzalovarelaalagna@gmail.com>
2025-10-07 11:44:31 -0300

    update imports

--- src/actors/validators/payment/validatePaymentInput.js
+++ src/actors/validators/payment/validatePaymentInput.js
@@ -7,17 +7,16 @@ import { validateStatus } from "./validateStatus.js";
 import{validatePaymentMethod} from './validatePaymentMethod.js'
 import { validateCurrency } from "./validateCurrency.js";
 import { validateAmount } from "./validateAmount.js";
+import { validatePaidAt } from "./validatePaidAt.js";
 
 export function validatePaymentInput(payment, updating = false) {
   console.log("validatePaymentInput, payment:", payment);
-
   // Assign default dueDate if missing
   if (!payment.dueDate) {
     const defaultDueDate = new Date();
     defaultDueDate.setMonth(defaultDueDate.getMonth() + 1);
     payment.dueDate = defaultDueDate;
   }
-
   // Check for required fields
   if (
     !payment.id &&
@@ -32,23 +31,12 @@ export function validatePaymentInput(payment, updating = false) {
     throw new AppError(PaymentErrors.CREATE.INVALID_DATA);
   }
   validateStatus(payment.status);
+  validateDueDate(payment, updating);
   validatePaymentMethod(payment.method);
   validateAmount(payment.amount);
   validateCurrency(payment.currency);
-  // Validate paidAt only allowed if status is 'paid'
-  if (payment.status !== "paid") {
-    if (payment.paidAt instanceof Date || typeof payment.paidAt === "string") {
-      if (updating) throw new AppError(PaymentErrors.UPDATE.INVALID_DATA);
-
-      console.log("throwing 6");
-      throw new AppError(PaymentErrors.CREATE.INVALID_DATA);
-    }
-  }
-
-  
-  
+  validatePaidAt(payment.paidAt, payment.status);  
   validateReference(payment.reference);
   validateNotes(payment.notes, updating)
-  validateDueDate(payment, updating);
   // ✅ No need to reject paidAt before dueDate — early payments are valid
 }
