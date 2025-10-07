718799bbc420ba4c52b15e54e6f51354e5e69476
piragna <gonzalovarelaalagna@gmail.com>
2025-10-07 11:37:25 -0300

    New validator created

---
+++ src/actors/validators/payment/validateStatus.js
@@ -0,0 +1,10 @@
+export function validateStatus(status){
+    
+      // Validate allowed status values
+      const validStatuses = ["pending", "paid", "failed"];
+      if (!validStatuses.includes(status)) {
+        if (updating) throw new AppError(PaymentErrors.UPDATE.INVALID_DATA);
+        console.log("throwing 1");
+        throw new AppError(PaymentErrors.CREATE.INVALID_DATA);
+      }
+}
