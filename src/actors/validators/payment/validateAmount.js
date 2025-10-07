94b07210c17efe733b771e6dcf739a99473fc7a8
piragna <gonzalovarelaalagna@gmail.com>
2025-10-07 11:38:35 -0300

    New validator created

---
+++ src/actors/validators/payment/validateAmount.js
@@ -0,0 +1,9 @@
+export function validateAmount(amount){
+    // Validate amount is a non-negative number
+      if (typeof amount !== "number" || amount < 0) {
+        if (updating) throw new AppError(PaymentErrors.UPDATE.INVALID_DATA);
+    
+        console.log("throwing 3");
+        throw new AppError(PaymentErrors.CREATE.INVALID_DATA);
+      }
+}
