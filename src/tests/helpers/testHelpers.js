7e1fa1fb52bb633d68061d319713a76a7dfaaefb
piragna <gonzalovarelaalagna@gmail.com>
2025-10-07 12:41:47 -0300

    added new verification

--- src/tests/helpers/testHelpers.js
+++ src/tests/helpers/testHelpers.js
@@ -44,5 +44,6 @@ export function utcDate(ymd) {
 }
 
 export function isValidUUID(str) {
+  if (typeof str !== "string") return false;
   return /^[0-9a-f]{8}-[0-9a-f]{4}-[1-5][0-9a-f]{3}-[89ab][0-9a-f]{3}-[0-9a-f]{12}$/i.test(str);
 }
