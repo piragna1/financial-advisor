cc04411994098217be9d766e19e482026bd49db5
piragna <gonzalovarelaalagna@gmail.com>
2025-10-08 18:43:52 -0300

    Creating test suite for tokenExists.js

---
+++ src/tests/unit/actors/auth/tokenExists.test.js
@@ -0,0 +1,13 @@
+/* 
+
+import { AppError } from '../../errors/AppError.js';
+import { AuthErrors } from '../../errors/authErrors.js';
+export function tokenExists(req) {
+    const header = req?.headers?.authorization;
+    if (typeof header !== 'string' ) {
+        throw new AppError(AuthErrors.TOKEN.MISSING_TOKEN);
+    }
+    if ( header.trim()==='')  throw new AppError(AuthErrors.TOKEN.MISSING_TOKEN);
+    return true;
+}
+    */
