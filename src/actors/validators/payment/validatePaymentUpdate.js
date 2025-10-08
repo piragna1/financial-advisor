e01723673f8c0b7dd47754a9898fb9b53af3bb4b
piragna <gonzalovarelaalagna@gmail.com>
2025-10-08 11:51:26 -0300

    removing debug log

--- src/actors/validators/payment/validatePaymentUpdate.js
+++ src/actors/validators/payment/validatePaymentUpdate.js
@@ -3,7 +3,6 @@ import { PaymentErrors } from "../../../errors/paymentErrors.js";
 import { isValidUUID } from "../../../tests/helpers/testHelpers.js";
 import { validatePaymentInput } from "./validatePaymentInput.js";
 export function validatePaymentUpdate(payment) {
-  console.log('validatePaymentUpdate')
   try {
     validatePaymentInput(payment, true);
   } catch (err) {
