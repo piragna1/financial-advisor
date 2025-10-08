069ef405d6135ff1cf59353d2d6703cf1f627537
piragna <gonzalovarelaalagna@gmail.com>
2025-10-08 11:43:48 -0300

    Added debug log

--- src/actors/validators/payment/validatePaymentUpdate.js
+++ src/actors/validators/payment/validatePaymentUpdate.js
@@ -3,8 +3,7 @@ import { PaymentErrors } from "../../../errors/paymentErrors.js";
 import { isValidUUID } from "../../../tests/helpers/testHelpers.js";
 import { validatePaymentInput } from "./validatePaymentInput.js";
 export function validatePaymentUpdate(payment) {
-
-
+  console.log('validatePaymentUpdate')
   try {
     validatePaymentInput(payment, true);
   } catch (err) {
