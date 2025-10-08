013db8bc5e02055b52b839afec1309002196f17e
piragna <gonzalovarelaalagna@gmail.com>
2025-10-08 18:21:29 -0300

    Modified implementation for extracting token from header.authorization

--- src/actors/auth/extractToken.js
+++ src/actors/auth/extractToken.js
@@ -1,3 +1,4 @@
 export function extractToken(req){
-    return req.headers.authorization;
+    const header = req.headers?.authorization;
+    return header?.startsWith('Bearer ') ? header.slice(7) : header;
 }
