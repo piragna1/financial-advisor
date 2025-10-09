3cd47b921c3dc9636f1f4242340800b03e16f949
piragna <gonzalovarelaalagna@gmail.com>
2025-10-09 11:48:39 -0300

    Conditional aggregated

--- src/actors/utils/hasEmoji.js
+++ src/actors/utils/hasEmoji.js
@@ -1,4 +1,5 @@
 export function hasEmoji(input){
+    if (typeof input !== 'string') throw new Error("Input is not a string")
     const emojiRegex =/[\u{1F600}-\u{1F64F}]|[\u{1F300}-\u{1F5FF}]|[\u{1F680}-\u{1F6FF}]|[\u{1F1E6}-\u{1F1FF}]|[\u{2600}-\u{26FF}]|[\u{2700}-\u{27BF}]/u;
     return emojiRegex.test(input);
 }
