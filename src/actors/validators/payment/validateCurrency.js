73a604c177a81edcdd6d7ab8ec89a2a1553ff2c8
piragna <gonzalovarelaalagna@gmail.com>
2025-10-09 15:28:40 -0300

    modified implementation

--- src/actors/validators/payment/validateCurrency.js
+++ src/actors/validators/payment/validateCurrency.js
@@ -1,22 +1,12 @@
 import { PaymentErrors } from "../../../errors/paymentErrors";
 import { AppError } from "../../../errors/appError";
-export function validateCurrency(currency, updating
-){
-    
-      // Validate currency format and allowed values
-      if (typeof currency !== "string" || currency.trim() === "") {
-        if (updating) throw new AppError(PaymentErrors.UPDATE.INVALID_DATA);
-    
-        console.log("throwing 4");
-        throw new AppError(PaymentErrors.CREATE.INVALID_DATA);
-      }
-    
-      const validCurrencies = ["USD", "ARS", "EUR"];
-      if (!validCurrencies.includes(currency)) {
-        if (updating) throw new AppError(PaymentErrors.UPDATE.INVALID_DATA);
-    
-        console.log("throwing 5");
-        throw new AppError(PaymentErrors.CREATE.INVALID_DATA);
-      }
-    
+export function validateCurrency(currency) {
+  // Validate currency format and allowed values
+  if (typeof currency !== "string" || currency.trim() === "") {
+    throw new AppError(PaymentErrors.CURRENCY.INVALID);
+  }
+  const validCurrencies = ["USD", "ARS", "EUR"];
+  if (!validCurrencies.includes(currency)) {
+    throw new AppError(PaymentErrors.CURRENCY.INVALID);
+  }
 }
