a877cf194aba2457a609261ba30a68c21fcb2ea7
piragna <gonzalovarelaalagna@gmail.com>
2025-10-09 12:17:14 -0300

    deleted file: checkUserUniqueness.js

--- src/actors/validators/auth/checkUserUniqueness.js
+++
@@ -1,7 +0,0 @@
-export async function checkUserUniqueness (email, users) {
-    const exist = users.some((user) => user['email'] === email);
-    return {
-        succes: exist,
-        message: exist ? 'Email is not valid' : 'Email available'
-    }
-}
