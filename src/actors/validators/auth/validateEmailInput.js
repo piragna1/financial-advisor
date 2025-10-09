ad182ffaf8ae62a77cee2262ff23ced3e8570d21
piragna <gonzalovarelaalagna@gmail.com>
2025-10-09 12:43:45 -0300

    Modified errors thrown

--- src/actors/validators/auth/validateEmailInput.js
+++ src/actors/validators/auth/validateEmailInput.js
@@ -3,12 +3,12 @@ import { AuthErrors } from "../../../errors/authErrors.js";
 
 export function validateEmailInput(email) {
   if (!email) {
-    throw new AppError(AuthErrors.MISSING_CREDENTIALS, 'Email is required');
+    throw new Error('Email is required');
   } else {
     if (email && typeof email !== "string") {
-      throw new AppError(AuthErrors.INVALID_INPUT, 'Email must be a string')
+      throw new Error('Email must be a string')
     } else if (email && !/^\S+@\S+\.\S+$/.test(email)) {
-      throw new AppError(AuthErrors.INVALID_INPUT, 'Email must be a string')
+      throw new Error('Email must be a string')
     }
   }
   return true;
