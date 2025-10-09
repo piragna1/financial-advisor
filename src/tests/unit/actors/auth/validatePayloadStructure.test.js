f24aca9bb25f3e042efb834c1c46a166fd806a8e
piragna <gonzalovarelaalagna@gmail.com>
2025-10-09 08:56:00 -0300

    Suite created

---
+++ src/tests/unit/actors/auth/validatePayloadStructure.test.js
@@ -0,0 +1,21 @@
+/*import { AppError } from "../../errors/AppError.js";
+import { AuthErrors } from "../../errors/authErrors.js";
+
+export function validatePayloadStructure(payload) {
+  if (!payload || typeof payload !== "object") {
+    throw new AppError(AuthErrors.TOKEN.INVALID_PAYLOAD);
+  }
+  const { sub, iat, exp } = payload;
+
+  if (!sub || typeof sub !== "string" || sub.trim() === "")
+    throw new AppError(AuthErrors.TOKEN.INVALID_PAYLOAD);
+
+  if (!iat || typeof iat !== "number" || iat === Infinity || iat <0)
+    throw new AppError(AuthErrors.TOKEN.INVALID_PAYLOAD);
+
+  if (!exp || typeof exp !== "number" || exp === Infinity || exp<0)
+    throw new AppError(AuthErrors.TOKEN.INVALID_PAYLOAD);
+
+  return true;
+}
+*/
