42154463db4dc84133e29f2cdccf3387bbc581ed
piragna <gonzalovarelaalagna@gmail.com>
2025-10-09 12:03:53 -0300

    Modified regex for considering whitespaces and control chars as not symbols

--- src/actors/utils/isNotSymbol.js
+++ src/actors/utils/isNotSymbol.js
@@ -1,5 +1,5 @@
 export function isNotSymbol(char){
     // Check if the character is alphanumeric or a space
   if (char == null || char === '') return true;
-  return /^[\p{L}'-]$/u.test(char);
+  return /^(?:\p{L}|'|-|\s)$/u.test(char);
 }
