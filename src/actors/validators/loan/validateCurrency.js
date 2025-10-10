ff3dbebe1b72224bb72cb377131a547caaad7529
piragna <gonzalovarelaalagna@gmail.com>
2025-10-10 13:04:30 -0300

    fixed  bug

--- src/actors/validators/loan/validateCurrency.js
+++ src/actors/validators/loan/validateCurrency.js
@@ -1,7 +1,7 @@
 export function validateCurrency(currency){
     if (
-    typeof loanData.currency !== "string" ||
-    !/^[A-Z]{3}$/.test(loanData.currency)
+    typeof currency !== "string" ||
+    !/^[A-Z]{3}$/.test(currency)
   )
     throw new Error("Currency must be a 3-letter ISO code (e.g., USD, EUR)");
 }
