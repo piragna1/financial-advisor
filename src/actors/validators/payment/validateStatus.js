fd1233cef5d750ac586d32975849d2499ad98e36
piragna <gonzalovarelaalagna@gmail.com>
2025-10-10 10:22:38 -0300

    modified error thrown

--- src/actors/validators/payment/validateStatus.js
+++ src/actors/validators/payment/validateStatus.js
@@ -6,6 +6,6 @@ export function validateStatus(status){
       // Validate allowed status values
       const validStatuses = ["pending", "paid", "failed"];
       if (!validStatuses.includes(status)) {
-        throw new AppError(PaymentErrors.CREATE.INVALID_DATA);
+        throw new AppError(PaymentErrors.STATUS.INVALID);
       }
 }
