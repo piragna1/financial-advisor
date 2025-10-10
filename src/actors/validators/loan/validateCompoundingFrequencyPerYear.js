db11a562200339a1a6256410f26a14bfbdbe44df
piragna <gonzalovarelaalagna@gmail.com>
2025-10-10 12:57:56 -0300

    Modified implementation

--- src/actors/validators/loan/validateCompoundingFrequencyPerYear.js
+++ src/actors/validators/loan/validateCompoundingFrequencyPerYear.js
@@ -1,8 +1,11 @@
-export function validateCompoundingFrequencyPerYear(compoundingFrequencyPerYear){
-    if (
-    typeof loanData.compoundingFrequencyPerYear !== "number" ||
-    loanData.compoundingFrequencyPerYear <= 0
-  )
-    throw new Error("Compounding frequency must be a positive number");
+import { AppError } from "../../../errors/AppError";
+import { LoanErrors } from "../../../errors/loanErrors";
+
+export function validateCompoundingFrequencyPerYear(compoundingFrequencyPerYear) {
+  if (typeof compoundingFrequencyPerYear !== "number" || isNaN(compoundingFrequencyPerYear))
+    throw new AppError(LoanErrors.COMPOUNDING_FREQUENCY_PER_YEAR.NOT_A_NUMBER);
+
+    if (compoundingFrequencyPerYear < 0)
+      throw new AppError(LoanErrors.COMPOUNDING_FREQUENCY_PER_YEAR.NEGATIVE);
 
 }
