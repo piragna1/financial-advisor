7790f09129b9e33d377220d420fcb840bfd8abae
piragna <gonzalovarelaalagna@gmail.com>
2025-10-13 09:42:17 -0300

    Modified error thrown's type.

--- src/actors/validators/loan/validateTermYears.js
+++ src/actors/validators/loan/validateTermYears.js
@@ -1,4 +1,7 @@
+import { AppError } from "../../../errors/AppError";
+import { LoanErrors } from "../../../errors/loanErrors";
+
 export function validateTermYears(termYears) {
   if (typeof termYears !== "number" || isNaN(termYears))
-    throw new Error("termYears must be a valid number");
+    throw new AppError(LoanErrors.VALIDATION.INVALID_TERM_YEARS);
 }
