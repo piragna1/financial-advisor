cdfa4be943e3a8e24286d2609366e2b546c1c0c3
piragna <gonzalovarelaalagna@gmail.com>
2025-10-13 09:16:01 -0300

    Modified error thrown's type.

--- src/actors/validators/loan/validateStartDate.js
+++ src/actors/validators/loan/validateStartDate.js
@@ -1,7 +1,10 @@
+import {AppError} from '../../../errors/AppError.js'
+import {LoanErrors} from '../../../errors/loanErrors.js'
+
 export function validateStartDate(startDate) {
     if (
         !(startDate instanceof Date) ||
         isNaN(startDate.getTime())
     )
-        throw new Error("startDate must be a valid Date object");
+        throw new AppError(LoanErrors.VALIDATION.INVALID_START_DATE);
 }
