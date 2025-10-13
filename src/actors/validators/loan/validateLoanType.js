ccf50971727107895d0f7488849904000034d33e
piragna <gonzalovarelaalagna@gmail.com>
2025-10-13 09:05:37 -0300

    modified implementation

--- src/actors/validators/loan/validateLoanType.js
+++ src/actors/validators/loan/validateLoanType.js
@@ -1,3 +1,5 @@
+import {AppError} from '../../../errors/AppError.js'
+import {LoanErrors, loanErrors} from '../../../errors/loanErrors.js'
 export function validateLoanType(loanType){
     const allowedLoanTypes = [
     "personal",
@@ -8,8 +10,8 @@ export function validateLoanType(loanType){
   ];
   //loanType is an allowed type and a string
   if (
-    typeof loanData.loanType !== "string" ||
-    !allowedLoanTypes.includes(loanData.loanType.toLowerCase())
+    typeof loanType !== "string" ||
+    !allowedLoanTypes.includes(loanType.toLowerCase())
   )
-    throw new Error(`Unsupported loan type: ${loanData.loanType}`);
+    throw new AppError(LoanErrors.VALIDATION.INVALID_LOAN_TYPE);
 }
