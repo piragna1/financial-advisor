db9676f87341993e59694449cee69c8a477daef6
piragna <gonzalovarelaalagna@gmail.com>
2025-10-13 09:09:18 -0300

    Modified implementation.

--- src/actors/validators/loan/validatePaymentFrequencyPerYear.js
+++ src/actors/validators/loan/validatePaymentFrequencyPerYear.js
@@ -1,7 +1,9 @@
-export function validatePaymentFrequencyPerYear(paymentFrequencyPerYear){
-    if (
-    typeof loanData.paymentFrequencyPerYear !== "number" ||
-    loanData.paymentFrequencyPerYear <= 0
-  )
-    throw new Error("Payment frequency must be a positive number");
+import { AppError } from "../../../errors/AppError";
+import { LoanErrors } from "../../../errors/loanErrors";
+
+export function validatePaymentFrequencyPerYear(paymentFrequencyPerYear) {
+  if (typeof paymentFrequencyPerYear !== "number" || isNaN(paymentFrequencyPerYear))
+    throw new AppError(LoanErrors.VALIDATION.INVALID_PAYMENT_FREQUENCY)
+  if (paymentFrequencyPerYear <= 0)
+    throw new AppError(LoanErrors.VALIDATION.INVALID_PAYMENT_FREQUENCY)
 }
