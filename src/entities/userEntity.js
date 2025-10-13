81c3f587d292456b2eedd1a99bf767cb3330a4c2
piragna <gonzalovarelaalagna@gmail.com>
2025-10-13 10:33:35 -0300

    Modified implementation.

--- src/entities/userEntity.js
+++ src/entities/userEntity.js
@@ -1,19 +1,19 @@
-import { AppError } from "../errors/AppError.js";
 import { AuthErrors } from "../errors/authErrors.js";
-import assert from "assert";
+import { AppError } from "../errors/AppError.js";
+import {UserErrors} from '../errors/userErrors.js'
 
 export function buildUserEntity({ id, email, hashedPassword }) {
 
 
   if (!id || !email || !hashedPassword) {
-    throw new AppError(AuthErrors.MISSING_CREDENTIALS);
+    throw new AppError(UserErrors.CREATE.INVALID_INPUT);
   }
   if (
     typeof id != "string" ||
     typeof email != "string" ||
     typeof hashedPassword != "string"
   ) {
-    throw new AppError(AuthErrors.INVALID_INPUT, "Format of data is invalid");
+    throw new AppError(UserErrors.CREATE.INVALID_INPUT);
   }
   const user = {
     id,
